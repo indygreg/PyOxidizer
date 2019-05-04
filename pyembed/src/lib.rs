51fe4d0ddeec2684550d4890611177aff2e92514
Gregory Szorc <gregory.szorc@gmail.com>
2019-05-04 14:31:42 -0700

    pyembed: remove some "extern crate"
    It isn't needed in 2018 edition.

--- pyembed/src/lib.rs
+++ pyembed/src/lib.rs
@@ -2,10 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-extern crate byteorder;
 #[macro_use]
 extern crate cpython;
-extern crate libc;
 extern crate python3_sys as pyffi;
 
 mod data;
