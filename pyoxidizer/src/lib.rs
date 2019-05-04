d73b2ffa63706ea3b179ea4035af1509a4dfe421
Gregory Szorc <gregory.szorc@gmail.com>
2019-05-04 16:20:56 -0700

    pyrepackager: remove serde_derive
    We don't need it in Rust 2018.

--- pyoxidizer/src/lib.rs
+++ pyoxidizer/src/lib.rs
@@ -2,9 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#[macro_use]
-extern crate serde_derive;
-
 pub mod analyze;
 pub mod pyrepackager;
 pub mod python_distributions;
