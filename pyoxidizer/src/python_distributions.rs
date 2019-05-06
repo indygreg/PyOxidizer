21c610524d0f56b3bb6100c446939fe1fa620e9a
Gregory Szorc <gregory.szorc@gmail.com>
2019-05-05 17:50:04 -0700

    python_distributions: add Windows distribution, update Linux
    These distros have version=1 in their PYTHON.json files.

--- pyoxidizer/src/python_distributions.rs
+++ pyoxidizer/src/python_distributions.rs
@@ -16,8 +16,13 @@ lazy_static! {
         let mut res: BTreeMap<&'static str, HostedDistribution> = BTreeMap::new();
 
         res.insert("x86_64-unknown-linux-gnu", HostedDistribution {
-            url: String::from("https://github.com/indygreg/python-build-standalone/releases/download/20190427/cpython-3.7.3-linux64-20190427T2308.tar.zst"),
-            sha256: String::from("0b30af0deb4852f2099c7905f80f55b70f7eec152cd19a3a65b577d4350ad47a"),
+            url: String::from("https://github.com/indygreg/python-build-standalone/releases/download/20190505/cpython-3.7.3-linux64-20190506T0025.tar.zst"),
+            sha256: String::from("837a685551b48ac3dc40a6b279c20a2ce96b15c2873ba0c537463e188c8e3d1b"),
+        });
+
+        res.insert("x86_64-pc-windows-msvc", HostedDistribution {
+            url: String::from("https://github.com/indygreg/python-build-standalone/releases/download/20190505/cpython-3.7.3-windows-amd64-20190506T0003.tar.zst"),
+            sha256: String::from("9db612991c1d58b117bb40a9f357d15b75cc9a4b4e476a65cf0ae7ce237be8xii30"),
         });
 
         res
