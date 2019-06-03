11bc0ed9661ae17cc37c9df184857c67cfb73e00
Gregory Szorc <gregory.szorc@gmail.com>
2019-06-02 20:13:44 -0700

    main: add some high level documentation about PyOxidizer
    And the copying of content from README.rst begins...

--- pyoxidizer/src/main.rs
+++ pyoxidizer/src/main.rs
@@ -4,7 +4,27 @@
 
 /*!
 
-PyOxidizer: Build and Distribute Python Applications
+`PyOxidizer` is a tool and library for producing binaries that embed
+Python.
+
+The over-arching goal of `PyOxidizer` is to make complex Python
+packaging and distribution problems simple so application maintainers
+can focus on building quality applications instead of toiling with
+build systems and packaging tools.
+
+`PyOxidizer` is capable of producing a self-contained executable containing
+a fully-featured Python interpreter and all Python modules required to run
+a Python application. On Linux, it is possible to create a fully static
+executable that doesn't even support dynamic loading and can run on nearly
+every Linux machine.
+
+The *Oxidizer* part of the name comes from Rust: binaries built with
+`PyOxidizer` are compiled from Rust and Rust code is responsible for
+managing the embedded Python interpreter and all its operations. But the
+existence of Rust should be invisible to many users, much like the fact
+that CPython (the official Python distribution available from www.python.org)
+is implemented in C. Rust is simply a tool to achieve an end goal, albeit
+a rather effective and powerful tool.
 
 # PyOxidizer Config Files
 
