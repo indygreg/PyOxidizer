4e96ba7f0c95fa4301de42f2cae353f5968454d1
Gregory Szorc <gregory.szorc@gmail.com>
2019-06-02 19:58:09 -0700

    pyoxidizer: rename library to pyoxidizerlib
    Mainly so we can have the binary supply the crate docs.

--- pyembed/build.rs
+++ pyembed/build.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use pyoxidizer::run_from_build;
+use pyoxidizerlib::run_from_build;
 
 fn main() {
     run_from_build("build.rs");
--- pyoxidizer/src/templates/pyembed-build.rs
+++ pyoxidizer/src/templates/pyembed-build.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use pyoxidizer::run_from_build;
+use pyoxidizerlib::run_from_build;
 
 fn main() {
     // run_from_build() performs the heavy work of embedding Python into
