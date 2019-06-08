6047a3f0d8bbac0f59305f57f1a11c111dd74d7a
Gregory Szorc <gregory.szorc@gmail.com>
2019-06-08 11:15:46 -0700

    pyrepackager: extract stdlib extensions explicit includes to standalone fn
    The march continues.

--- pyoxidizer/src/pyrepackager/repackage.rs
+++ pyoxidizer/src/pyrepackager/repackage.rs
@@ -284,6 +284,27 @@ fn resolve_stdlib_extensions_policy(
     res
 }
 
+fn resolve_stdlib_extensions_explicit_includes(
+    dist: &PythonDistributionInfo,
+    includes: &Vec<String>,
+) -> Vec<PythonResourceEntry> {
+    let mut res = Vec::new();
+
+    for name in includes {
+        if let Some(modules) = &dist.extension_modules.get(name) {
+            res.push(PythonResourceEntry {
+                action: ResourceAction::Add,
+                resource: PythonResource::ExtensionModule {
+                    name: name.clone(),
+                    module: modules[0].clone(),
+                },
+            });
+        }
+    }
+
+    res
+}
+
 /// Resolves a Python packaging rule to resources to package.
 fn resolve_python_packaging(
     logger: &slog::Logger,
@@ -298,17 +319,7 @@ fn resolve_python_packaging(
         }
 
         PythonPackaging::StdlibExtensionsExplicitIncludes { includes } => {
-            for name in includes {
-                if let Some(modules) = &dist.extension_modules.get(name) {
-                    res.push(PythonResourceEntry {
-                        action: ResourceAction::Add,
-                        resource: PythonResource::ExtensionModule {
-                            name: name.clone(),
-                            module: modules[0].clone(),
-                        },
-                    });
-                }
-            }
+            res.extend(resolve_stdlib_extensions_explicit_includes(dist, includes));
         }
 
         PythonPackaging::StdlibExtensionsExplicitExcludes { excludes } => {
