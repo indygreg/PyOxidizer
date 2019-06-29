b70196078ac2a95beac665e80027c1caea72856b
Gregory Szorc <gregory.szorc@gmail.com>
2019-06-29 12:59:21 -0700

    pyrepackager: remove some unused constants
    These are a relic of an ancient era.

--- pyoxidizer/src/pyrepackager/mod.rs
+++ pyoxidizer/src/pyrepackager/mod.rs
@@ -7,30 +7,3 @@ pub mod config;
 pub mod dist;
 pub mod fsscan;
 pub mod repackage;
-
-#[allow(unused)]
-const STDLIB_NONTEST_IGNORE_DIRS: &[&str] = &[
-    // The config directory describes how Python was built. It isn't relevant.
-    "config",
-    // ensurepip is useful for Python installs, which we're not. Ignore it.
-    "ensurepip",
-    // We don't care about the IDLE IDE.
-    "idlelib",
-    // lib2to3 is used for python Python 2 to Python 3. While there may be some
-    // useful generic functions in there for rewriting Python source, it is
-    // quite large. So let's not include it.
-    "lib2to3",
-    // site-packages is where additional packages go. We don't use it.
-    "site-packages",
-];
-
-#[allow(unused)]
-const STDLIB_IGNORE_FILES: &[&str] = &[
-    // These scripts are used for building macholib. They don't need to be in
-    // the standard library.
-    "ctypes/macholib/fetch_macholib",
-    "ctypes/macholib/etch_macholib.bat",
-    "ctypes/macholib/README.ctypes",
-    "distutils/README",
-    "wsgiref.egg-info",
-];
