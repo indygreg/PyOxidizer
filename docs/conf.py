f44a2c60380b64766fa76a38aa0b87f3cfcd0221
Gregory Szorc <gregory.szorc@gmail.com>
2019-08-16 20:53:13 -0700

    docs: dynamically resolve version string
    So it doesn't get out of sync with reality.

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,22 @@
+import os
+import re
+
+HERE = os.path.dirname(__file__)
+ROOT = os.path.dirname(HERE)
+
+release = 'unknown'
+
+with open(os.path.join(ROOT, 'pyoxidizer', 'Cargo.toml'), 'r') as fh:
+    for line in fh:
+        m = re.match('^version = \"([^"]+)"', line)
+        if m:
+            release = m.group(1)
+            break
+
+
 project = 'PyOxidizer'
 copyright = '2019, Gregory Szorc'
 author = 'Gregory Szorc'
-release = '0.1'
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
