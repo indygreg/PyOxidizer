0aed00e3ca6a9f6b4e6a460cb1b8bfebf3bb5ac0
Gregory Szorc <gregory.szorc@gmail.com>
2019-11-16 16:37:17 -0800

    pyembed: reference run_from_build in new location

--- pyembed/build.rs
+++ pyembed/build.rs
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use pyoxidizerlib::app_packaging::repackage::run_from_build;
 use pyoxidizerlib::logging::logger_from_env;
-use pyoxidizerlib::pyrepackager::repackage::run_from_build;
 use std::env;
 use std::path::PathBuf;
 
