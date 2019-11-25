bc54fba9d356fa642e4960e3787f51c313ecc552
John Vandenberg <jayvdb@gmail.com>
2019-11-25 07:29:44 +0700

    distutils: Discard cffi's libffi on MSVC
    cffi includes a near replica of the CPython 3.7's libffi on MSVC,
    which needs to be discard to avoid duplicate syms.

--- pyoxidizer/src/distutils/_msvccompiler.py
+++ pyoxidizer/src/distutils/_msvccompiler.py
@@ -588,6 +588,11 @@ class MSVCCompiler(CCompiler) :
         # directory that doesn't outlive this process.
         object_paths = []
         for i, o in enumerate(objects):
+            if 'libffi_msvc' in o:
+                print('Ignored static {}'.format(o))
+                # https://github.com/indygreg/python-build-standalone/issues/23
+                # cffi includes a near replica of CPython 3.7's custom libffi.
+                continue
             o = o + '.static'
             p = os.path.join(dest_path, '%s.%d.o' % (name, i))
             shutil.copyfile(o, p)
