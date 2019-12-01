20cecc73d22ae97392d7dcc7e37c492c8b6cf105
Gregory Szorc <gregory.szorc@gmail.com>
2019-11-30 18:48:51 -0800

    ci: disable --pip-install functionality
    The autogenerated pyoxidizer.bzl file doesn't work with the new
    install rules just quite yet. Let's disable the functionality until
    it is reimplemented. This should hopefully be soon, as I don't like
    regressions living for long...

--- ci/pyapp.py
+++ ci/pyapp.py
@@ -1,11 +1,13 @@
-import appdirs
-import _cffi_backend
+# TODO uncomment once the auto-generated pyoxidizer.bzl contains proper
+# syntax.
+#import appdirs
+#import _cffi_backend
 # Allow AttributeError rather than ImportError
 # as this indirectly triggers a missing __file__
 # https://github.com/dabeaz/ply/issues/216
-try:
-    import zero_buffer
-except AttributeError:
-    pass
+#try:
+#    import zero_buffer
+#except AttributeError:
+#    pass
 
 print("hello, world")
