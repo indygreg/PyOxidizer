4a6242a760deb4a72ea0655ee3c3a60322a09ba2
Gregory Szorc <gregory.szorc@gmail.com>
2019-12-01 13:58:35 -0800

    py_packaging: add method for compiling a Python library
    This somewhat reimplements logic that exists in app_packaging.
    But this way things are tied to the new types and are more
    generic.

--- pyoxidizer/src/py_packaging/binary.rs
+++ pyoxidizer/src/py_packaging/binary.rs
@@ -2,9 +2,13 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use slog::warn;
+use tempdir::TempDir;
+
 use super::config::{EmbeddedPythonConfig, RunMode};
 use super::distribution::ParsedPythonDistribution;
 use super::embedded_resource::EmbeddedPythonResourcesPrePackaged;
+use super::libpython::link_libpython;
 
 /// A self-contained Python executable before it is compiled.
 #[derive(Debug, Clone)]
@@ -13,4 +17,55 @@ pub struct PreBuiltPythonExecutable {
     pub resources: EmbeddedPythonResourcesPrePackaged,
     pub config: EmbeddedPythonConfig,
     pub run_mode: RunMode,
+}
+
+impl PreBuiltPythonExecutable {
+    /// Build a Python library suitable for linking.
+    ///
+    /// This will take the underlying distribution, resources, and
+    /// configuration and produce a new executable binary.
+    pub fn build_libpython(
+        &self,
+        logger: &slog::Logger,
+        host: &str,
+        target: &str,
+        opt_level: &str,
+    ) -> Result<PythonLibrary, String> {
+        let resources = self.resources.package(&self.distribution.python_exe)?;
+
+        let temp_dir = TempDir::new("pyoxidizer-build-exe").or_else(|e| Err(e.to_string()))?;
+        let temp_dir_path = temp_dir.path();
+
+        warn!(
+            logger,
+            "generating custom link library containing Python..."
+        );
+        let library_info = link_libpython(
+            logger,
+            &self.distribution,
+            &resources,
+            &temp_dir_path,
+            host,
+            target,
+            opt_level,
+        );
+
+        let mut cargo_metadata: Vec<String> = Vec::new();
+        cargo_metadata.extend(library_info.cargo_metadata);
+
+        let data = std::fs::read(&library_info.path).or_else(|e| Err(e.to_string()))?;
+
+        Ok(PythonLibrary {
+            pre_built: self.clone(),
+            data,
+            cargo_metadata,
+        })
+    }
+}
+
+/// A self-contained Python executable after it is built.
+pub struct PythonLibrary {
+    pub pre_built: PreBuiltPythonExecutable,
+    pub data: Vec<u8>,
+    pub cargo_metadata: Vec<String>,
 }
