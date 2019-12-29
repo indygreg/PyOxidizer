a34e3d4c1cfc775ba971028ea4d93b010322888b
Gregory Szorc <gregory.szorc@gmail.com>
2019-12-29 10:56:25 -0800

    py_packaging: implement logic for resolving resource names from files
    This was already implemented in repackage.rs. We reimplement it in a
    more standalone manner.

--- pyoxidizer/src/py_packaging/filtering.rs
+++ pyoxidizer/src/py_packaging/filtering.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use slog::warn;
 use std::collections::{BTreeMap, BTreeSet};
 use std::fs::File;
@@ -25,6 +25,36 @@ pub fn read_resource_names_file(path: &Path) -> Result<BTreeSet<String>> {
     }
 
     Ok(res)
+}
+
+pub fn resolve_resource_names_from_files(
+    files: &[&Path],
+    glob_files: &[&str],
+) -> Result<BTreeSet<String>> {
+    let mut include_names = BTreeSet::new();
+
+    for path in files {
+        let new_names = read_resource_names_file(path)?;
+        include_names.extend(new_names);
+    }
+
+    for pattern in glob_files {
+        let mut new_names = BTreeSet::new();
+
+        for entry in glob::glob(pattern)? {
+            new_names.extend(read_resource_names_file(&entry?)?);
+        }
+
+        if new_names.is_empty() {
+            return Err(anyhow!(
+                "glob filter resolves to empty set; are you sure the glob pattern is correct?"
+            ));
+        }
+
+        include_names.extend(new_names);
+    }
+
+    Ok(include_names)
 }
 
 pub fn filter_btreemap<V>(
