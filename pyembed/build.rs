fe016d036c961e878ae80da2fb5248b16a43de8c
Gregory Szorc <gregory.szorc@gmail.com>
2020-01-27 20:03:46 -0800

    pyembed: add documentation from pyembed-build.rs template
    I'd like to delete the templatized version of this file...

--- pyembed/build.rs
+++ pyembed/build.rs
@@ -2,6 +2,27 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+/*!
+Build script to embed Python in a binary.
+
+The goal of this build script is to emit metadata to tell the build
+system how to embed a Python interpreter into a binary.
+
+This is done by printing the content of an artifact (cargo_metadata.txt)
+produced by PyOxidizer. This artifact in turn references other build
+artifacts.
+
+The following strategies exist for obtaining the build artifacts needed
+by this crate:
+
+1. Call `pyoxidizer run-build-script` and use its output verbatim.
+2. Call into the PyOxidizer library directly to perform the equivalent
+   of `pyoxidizer run-build-script`. (See bottom of file for an example.)
+3. Build artifacts out-of-band and consume them manually in this script
+   (e.g. by calling `pyoxidizer build` and then reading the generated
+   `cargo_metadata.txt` file manually.)
+*/
+
 use {
     std::env,
     std::path::{Path, PathBuf},
