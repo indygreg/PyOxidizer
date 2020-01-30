5e24f04566d678dd6452ce52469141666df32b81
Gregory Szorc <gregory.szorc@gmail.com>
2020-01-29 22:05:34 -0800

    pyembed: tweak crate documentation
    Now that this crate is standalone and we intend to publish it, we should
    start thinking about shoring up the docs!

--- pyembed/src/lib.rs
+++ pyembed/src/lib.rs
@@ -3,14 +3,21 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 /*!
-Manage an embedded Python interpreter.
+Control an embedded Python interpreter.
 
-The `pyembed` crate contains functionality for managing a Python interpreter
-embedded in the current binary. This crate is typically used along with
-[PyOxidizer](https://github.com/indygreg/PyOxidizer) for producing
-self-contained binaries containing Python.
+The `pyembed` crate contains functionality for controlling an embedded
+Python interpreter running in the current process.
 
-The most important types are [`PythonConfig`](struct.PythonConfig.html) and
+While this crate is conceptually generic and can be used as a high-level
+manager of an embedded Python interpreter (it has a high-level API that
+makes running an embedded Python interpreter relatively easy), the crate
+was designed for use with [PyOxidizer](https://github.com/indygreg/PyOxidizer).
+If you are leveraging the advanced features like the module importer that can
+import modules from memory using 0-copy, you probably want to use this crate
+with `PyOxidizer`.
+
+The most important types in this crate are
+[`PythonConfig`](struct.PythonConfig.html) and
 [`MainPythonInterpreter`](struct.MainPythonInterpreter.html). A `PythonConfig`
 defines how a Python interpreter is to behave. A `MainPythonInterpreter`
 creates and manages that interpreter and serves as a high-level interface for
