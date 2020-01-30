815e5c4dccc6d9eea2a2096cd1c173a5a48a36dd
Gregory Szorc <gregory.szorc@gmail.com>
2020-01-29 22:16:05 -0800

    pyembed: collapse ifs to placate clippy

--- pyembed/src/osutils.rs
+++ pyembed/src/osutils.rs
@@ -80,10 +80,8 @@ fn resolve_linux_distro() -> LinuxDistroVariant {
                 } else if line.contains("rhel") || line.contains("fedora") {
                     return LinuxDistroVariant::RedHat;
                 }
-            } else if line.starts_with("ID=") {
-                if line.contains("fedora") {
-                    return LinuxDistroVariant::RedHat;
-                }
+            } else if line.starts_with("ID=") && line.contains("fedora") {
+                return LinuxDistroVariant::RedHat;
             }
         }
     }
