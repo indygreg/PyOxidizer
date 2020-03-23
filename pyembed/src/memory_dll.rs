12e8c540aecbfbbc3bcd556afeb33e10e0f538fc
Gregory Szorc <gregory.szorc@gmail.com>
2020-03-22 18:56:07 -0700

    pyembed: use MemoryLoadLibraryEx
    We need to use this variant in order to follow library dependencies.
    Although we don't yet implement this functionality.
    
    Our default functions should be the same as those implemented by
    MemoryModule.c.
    
    As part of implementing this, I discovered some bugs in
    memory-module-sys's types. I corrected those in that crate
    (I'm the maintainer) and published a new version.
    
    As part of this, we also limit features of the winapi crate so
    the build is much, much faster.

--- pyembed/src/memory_dll.rs
+++ pyembed/src/memory_dll.rs
@@ -5,19 +5,82 @@
 /*! Functionality for loading Windows DLLs from memory. */
 
 use {
-    memory_module_sys::{MemoryFreeLibrary, MemoryGetProcAddress, MemoryLoadLibrary},
+    memory_module_sys::{
+        MemoryFreeLibrary, MemoryGetProcAddress, MemoryLoadLibraryEx, HCUSTOMMODULE,
+    },
     std::ffi::{c_void, CStr},
-    winapi::shared::minwindef::FARPROC,
+    winapi::shared::basetsd::SIZE_T,
+    winapi::shared::minwindef::{BOOL, DWORD, FARPROC, HINSTANCE__, LPVOID},
+    winapi::shared::ntdef::LPCSTR,
+    winapi::um::libloaderapi::{FreeLibrary, GetProcAddress, LoadLibraryA},
+    winapi::um::memoryapi::{VirtualAlloc, VirtualFree},
 };
 
 pub(crate) unsafe fn load_library(data: &[u8]) -> *const c_void {
-    MemoryLoadLibrary(data.as_ptr() as *const c_void, data.len())
+    MemoryLoadLibraryEx(
+        data.as_ptr() as *const c_void,
+        data.len(),
+        default_alloc,
+        default_free,
+        default_load_library,
+        default_get_proc_address,
+        default_free_library,
+        std::ptr::null_mut(),
+    )
 }
 
-pub(crate) unsafe fn free_library(module: *const c_void) -> c_void {
-    MemoryFreeLibrary(module)
+pub(crate) unsafe fn free_library(module: *const c_void) {
+    MemoryFreeLibrary(module);
 }
 
 pub(crate) unsafe fn get_proc_address(module: *const c_void, name: &CStr) -> FARPROC {
     MemoryGetProcAddress(module, name.as_ptr())
+}
+
+#[no_mangle]
+extern "C" fn default_alloc(
+    address: LPVOID,
+    size: SIZE_T,
+    allocation_type: DWORD,
+    protect: DWORD,
+    _user_data: *mut c_void,
+) -> LPVOID {
+    unsafe { VirtualAlloc(address, size, allocation_type, protect) }
+}
+
+#[no_mangle]
+extern "C" fn default_free(
+    address: LPVOID,
+    size: SIZE_T,
+    free_type: DWORD,
+    _user_data: *mut c_void,
+) -> BOOL {
+    unsafe { VirtualFree(address, size, free_type) }
+}
+
+#[no_mangle]
+extern "C" fn default_load_library(filename: LPCSTR, _user_data: *mut c_void) -> HCUSTOMMODULE {
+    let result = unsafe { LoadLibraryA(filename) };
+
+    if result.is_null() {
+        std::ptr::null() as HCUSTOMMODULE
+    } else {
+        result as HCUSTOMMODULE
+    }
+}
+
+#[no_mangle]
+extern "C" fn default_get_proc_address(
+    module: HCUSTOMMODULE,
+    name: LPCSTR,
+    _user_data: *mut c_void,
+) -> FARPROC {
+    unsafe { GetProcAddress(module as *mut HINSTANCE__, name) }
+}
+
+#[no_mangle]
+extern "C" fn default_free_library(module: HCUSTOMMODULE, _user_data: *mut c_void) {
+    unsafe {
+        FreeLibrary(module as *mut HINSTANCE__);
+    }
 }
