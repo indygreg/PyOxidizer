9b9c753489302cd3b0ac4570b5297192c310b459
Gregory Szorc <gregory.szorc@gmail.com>
2020-04-27 14:42:48 -0700

    pyembed: add test for find_spec() and missing modules

--- pyembed/src/test/importer.rs
+++ pyembed/src/test/importer.rs
@@ -5,12 +5,34 @@
 use {
     crate::{MainPythonInterpreter, OxidizedPythonInterpreterConfig},
     anyhow::Result,
-    cpython::ObjectProtocol,
+    cpython::{ObjectProtocol, PyObject},
 };
+
+fn new_interpreter<'python, 'interpreter, 'resources>(
+) -> Result<MainPythonInterpreter<'python, 'interpreter, 'resources>> {
+    let mut config = OxidizedPythonInterpreterConfig::default();
+    config.oxidized_importer = true;
+    let interp = MainPythonInterpreter::new(config)?;
+
+    Ok(interp)
+}
+
+fn get_importer(interp: &mut MainPythonInterpreter) -> Result<PyObject> {
+    let py = interp.acquire_gil().unwrap();
+
+    let sys = py.import("sys").unwrap();
+    let meta_path = sys.get(py, "meta_path").unwrap();
+    assert_eq!(meta_path.len(py).unwrap(), 2);
+
+    let importer = meta_path.get_item(py, 0).unwrap();
+    assert_eq!(importer.get_type(py).name(py), "PyOxidizerFinder");
+
+    Ok(importer)
+}
 
 /// We can load our oxidized importer with no resources.
 #[test]
-fn test_no_resources() -> Result<()> {
+fn no_resources() -> Result<()> {
     let mut config = OxidizedPythonInterpreterConfig::default();
     config.oxidized_importer = true;
     let mut interp = MainPythonInterpreter::new(config)?;
@@ -29,6 +51,29 @@ fn test_no_resources() -> Result<()> {
     assert!(loader
         .to_string()
         .contains("_frozen_importlib.BuiltinImporter"));
+
+    Ok(())
+}
+
+/// find_spec() returns None on missing module.
+#[test]
+fn find_spec_missing() -> Result<()> {
+    let mut interp = new_interpreter()?;
+    let importer = get_importer(&mut interp)?;
+    let py = interp.acquire_gil().unwrap();
+
+    assert_eq!(
+        importer
+            .call_method(py, "find_spec", ("missing_package", py.None()), None)
+            .unwrap(),
+        py.None()
+    );
+    assert_eq!(
+        importer
+            .call_method(py, "find_spec", ("foo.bar", py.None()), None)
+            .unwrap(),
+        py.None()
+    );
 
     Ok(())
 }
