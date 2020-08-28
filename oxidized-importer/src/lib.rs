378cc45854e6e807cb1474cb0572a38a81f96f46
Gregory Szorc <gregory.szorc@gmail.com>
2020-08-28 15:15:02 -0700

    oxidized-importer: disable functionality in clippy mode
    Clippy adds no value here and gets confused by crate feature mismatch.
    We might be able to fix this via Cargo.toml or build script magic. But
    IMO it isn't worth it.
    
    With this change, `cargo clippy` is clean for the entire workspace!

--- oxidized-importer/src/lib.rs
+++ oxidized-importer/src/lib.rs
@@ -2,8 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// clippy doesn't know about what features to pass to dependent crates.
+// So pyembed isn't exporting the correct symbols and clippy will barf
+// due to unbound symbols. So we suppress clippy as a workaround.
+
+#[cfg(not(feature = "cargo-clippy"))]
 use python3_sys as pyffi;
 
+#[cfg(not(feature = "cargo-clippy"))]
 #[no_mangle]
 #[allow(non_snake_case)]
 pub unsafe extern "C" fn PyInit_oxidized_importer() -> *mut pyffi::PyObject {
