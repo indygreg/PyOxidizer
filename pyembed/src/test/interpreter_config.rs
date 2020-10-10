indygreg/PyOxidizer

commit 044ec18352a8a8dd6c7f307fa8841c0b1c3ea5db

pyembed: establish new types to hold full Python interpreter config
Python 3.8 introduced a ton of new APIs to control interpreter
initialization and configuration.

This commit introduces new types that define the full set of tweakable
configs that now exist.

To reinforce the separation between core Python configs and our
higher-level features, we use different types to represent the different
sets of config options. We keep the existing PythonConfig type
around and teach it to cast into our new type. The new type is
used to configure the Python interpreter.

There's still a bit of work to do here. We will need to teach
Starlark about the new knobs for example. And we'll want to
clean up the higher-level type a bit, adding a Default
implementation, etc. We also likely want to look at the new
run APIs, which are supported in PyConfig. That could probably
simplify our execution code a bit if we could leverage those.

pyembed/src/config.rs
@@ -4,7 +4,12 @@
 
 //! Data structures for configuring a Python interpreter.
 
-use {python3_sys as pyffi, std::ffi::CString};
+use {
+    libc::c_ulong,
+    python3_sys as pyffi,
+    std::ffi::{CString, OsString},
+    std::path::PathBuf,
+};
 
 /// Defines which allocator to use for the raw domain.
 #[derive(Clone, Debug)]
@@ -230,6 +235,407 @@ impl Default for PythonConfig {
             terminfo_resolution: TerminfoResolution::Dynamic,
             write_modules_directory_env: None,
             run: PythonRunMode::None,
+        }
+    }
+}
+
+/// Defines the profile to use to configure a Python interpreter.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum PythonInterpreterProfile {
+    /// Python is isolated from the system.
+    ///
+    /// See https://docs.python.org/3/c-api/init_config.html#isolated-configuration.
+    Isolated,
+
+    /// Python interpreter behaves like `python`.
+    ///
+    /// See https://docs.python.org/3/c-api/init_config.html#python-configuration.
+    Python,
+}
+
+impl Default for PythonInterpreterProfile {
+    fn default() -> Self {
+        PythonInterpreterProfile::Isolated
+    }
+}
+
+/// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.allocator.
+#[derive(Clone, Copy, Debug)]
+pub enum Allocator {
+    NotSet = 0,
+    Default = 1,
+    Debug = 2,
+    Malloc = 3,
+    MallocDebug = 4,
+    PyMalloc = 5,
+    PyMallocDebug = 6,
+}
+
+/// Holds values for coerce_c_locale.
+///
+/// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.coerce_c_locale.
+#[derive(Clone, Copy, Debug)]
+pub enum CoerceCLocale {
+    LCCtype = 1,
+    C = 2,
+}
+
+/// Defines what to do when comparing bytes with str.
+///
+/// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.bytes_warning.
+#[derive(Clone, Copy, Debug)]
+pub enum BytesWarning {
+    None = 0,
+    Warn = 1,
+    Raise = 2,
+}
+
+/// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.check_hash_pycs_mode.
+#[derive(Clone, Copy, Debug)]
+pub enum CheckHashPYCsMode {
+    Always,
+    Never,
+    Default,
+}
+
+/// Optimization level for bytecode.
+///
+/// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.optimization_level.
+#[derive(Clone, Copy, Debug)]
+pub enum OptimizationLevel {
+    Zero = 0,
+    One = 1,
+    Two = 2,
+}
+
+/// Holds configuration of a Python interpreter.
+///
+/// This struct holds fields that are exposed by `PyPreConfig` and
+/// `PyConfig` in the CPython API.
+///
+/// Other than the profile (which is used to initialize instances of
+/// `PyPreConfig` and `PyConfig`), all fields are optional. Only fields
+/// with `Some(T)` will be updated from the defaults.
+#[derive(Clone, Debug, Default)]
+pub struct PythonInterpreterConfig {
+    /// Profile to use to initialize pre-config and config state of interpreter.
+    pub profile: PythonInterpreterProfile,
+
+    // The following fields are from PyPreConfig or are shared with PyConfig.
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.allocator.
+    pub allocator: Option<Allocator>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.configure_locale.
+    pub configure_locale: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.coerce_c_locale.
+    pub coerce_c_locale: Option<CoerceCLocale>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.coerce_c_locale_warn.
+    pub coerce_c_locale_warn: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.dev_mode.
+    pub development_mode: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.isolated.
+    pub isolated: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.legacy_windows_fs_encoding.
+    pub legacy_windows_fs_encoding: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.parse_argv.
+    pub parse_argv: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.use_environment.
+    pub use_environment: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyPreConfig.utf8_mode.
+    pub utf8_mode: Option<bool>,
+    // The following fields are from PyConfig.
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.argv.
+    pub argv: Option<Vec<OsString>>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.base_exec_prefix.
+    pub base_exec_prefix: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.base_executable.
+    pub base_executable: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.base_prefix.
+    pub base_prefix: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.buffered_stdio.
+    pub buffered_stdio: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.bytes_warning.
+    pub bytes_warning: Option<BytesWarning>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.check_hash_pycs_mode.
+    pub check_hash_pycs_mode: Option<CheckHashPYCsMode>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.configure_c_stdio.
+    pub configure_c_stdio: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.dump_refs.
+    pub dump_refs: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.exec_prefix.
+    pub exec_prefix: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.executable.
+    pub executable: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.faulthandler.
+    pub fault_handler: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.filesystem_encoding.
+    pub filesystem_encoding: Option<String>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.filesystem_errors.
+    pub filesystem_errors: Option<String>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.hash_seed.
+    pub hash_seed: Option<c_ulong>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.home.
+    pub home: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.import_time.
+    pub import_time: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.inspect.
+    pub inspect: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.install_signal_handlers.
+    pub install_signal_handlers: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.interactive.
+    pub interactive: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.legacy_windows_stdio.
+    pub legacy_windows_stdio: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.malloc_stats.
+    pub malloc_stats: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.pythonpath_env.
+    pub python_path_env: Option<String>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.module_search_paths.
+    pub module_search_paths: Option<Vec<PathBuf>>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.optimization_level.
+    pub optimization_level: Option<OptimizationLevel>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.parser_debug.
+    pub parser_debug: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.pathconfig_warnings.
+    pub pathconfig_warnings: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.prefix.
+    pub prefix: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.program_name.
+    pub program_name: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.pycache_prefix.
+    pub pycache_prefix: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.quiet.
+    pub quiet: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.run_command.
+    pub run_command: Option<String>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.run_filename.
+    pub run_filename: Option<PathBuf>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.run_module.
+    pub run_module: Option<String>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.show_alloc_count.
+    pub show_alloc_count: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.show_ref_count.
+    pub show_ref_count: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.site_import.
+    pub site_import: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.skip_source_first_line.
+    pub skip_first_source_line: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.stdio_encoding.
+    pub stdio_encoding: Option<String>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.stdio_errors.
+    pub stdio_errors: Option<String>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.tracemalloc.
+    pub tracemalloc: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.user_site_directory.
+    pub user_site_directory: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.verbose.
+    pub verbose: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.warnoptions.
+    pub warn_options: Option<Vec<String>>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.write_bytecode.
+    pub write_bytecode: Option<bool>,
+
+    /// See https://docs.python.org/3/c-api/init_config.html#c.PyConfig.xoptions.
+    pub x_options: Option<Vec<String>>,
+}
+
+/// Configure a Python interpreter.
+///
+/// This type defines the configuration of a Python interpreter. It is used
+/// to initialize a Python interpreter embedded in the current process.
+///
+/// The type contains a reference to a `PythonInterpreterConfig` instance,
+/// which is an abstraction over the low-level C structs that Python uses during
+/// interpreter initialization.
+///
+/// The `PythonInterpreterConfig` has a single non-optional field: `profile`.
+/// This defines the defaults for various fields of the `PyPreConfig` and
+/// `PyConfig` instances that are initialized as part of interpreter
+/// initialization. See
+/// https://docs.python.org/3/c-api/init_config.html#isolated-configuration for
+/// more.
+///
+/// During interpreter initialization, we produce a `PyPreConfig` and
+/// `PyConfig` derived from this type. Config settings are applied in
+/// layers. First, we use the `PythonInterpreterConfig.profile` to derive
+/// a default instance given a profile. Next, we override fields if the
+/// `PythonInterpreterConfig` has `Some(T)` value set. Finally, we populate
+/// some fields if they are missing but required for the given configuration.
+/// For example, when in *isolated* mode, we set `program_name` and `home`
+/// unless an explicit value was provided in the `PythonInterpreterConfig`.
+///
+/// Generally speaking, the `PythonInterpreterConfig` exists to hold
+/// configuration that is defined in the CPython initialization and
+/// configuration API and `OxidizedPythonInterpreterConfig` exists to
+/// hold higher-level configuration for features specific to this crate.
+#[derive(Clone, Debug)]
+pub struct OxidizedPythonInterpreterConfig {
+    /// Low-level configuration of Python interpreter.
+    pub interpreter_config: PythonInterpreterConfig,
+
+    /// Allocator to use for Python's raw allocator.
+    pub raw_allocator: PythonRawAllocator,
+
+    /// Whether to install our custom meta path importer on interpreter init.
+    pub oxidized_importer: bool,
+
+    /// Whether to install the default `PathFinder` meta path finder.
+    pub filesystem_importer: bool,
+
+    /// Reference to packed resources data.
+    ///
+    /// The referenced data contains Python module data. It likely comes from an
+    /// `include_bytes!(...)` of a file generated by PyOxidizer.
+    ///
+    /// The format of the data is defined by the ``python-packed-resources``
+    /// crate. The data will be parsed as part of initializing the custom
+    /// meta path importer during interpreter initialization.
+    pub packed_resources: Option<&'static [u8]>,
+
+    /// Extra extension modules to make available to the interpreter.
+    ///
+    /// The values will effectively be passed to ``PyImport_ExtendInitTab()``.
+    pub extra_extension_modules: Option<Vec<ExtensionModule>>,
+
+    /// Whether to set sys.argvb with bytes versions of process arguments.
+    ///
+    /// On Windows, bytes will be UTF-16. On POSIX, bytes will be raw char*
+    /// values passed to `int main()`.
+    pub argvb: bool,
+
+    /// Whether to set sys.frozen=True.
+    ///
+    /// Setting this will enable Python to emulate "frozen" binaries, such as
+    /// those used by PyInstaller.
+    pub sys_frozen: bool,
+
+    /// Whether to set sys._MEIPASS to the directory of the executable.
+    ///
+    /// Setting this will enable Python to emulate PyInstaller's behavior
+    /// of setting this attribute.
+    pub sys_meipass: bool,
+
+    /// How to resolve the `terminfo` database.
+    pub terminfo_resolution: TerminfoResolution,
+
+    /// Environment variable holding the directory to write a loaded modules file.
+    ///
+    /// If this value is set and the environment it refers to is set,
+    /// on interpreter shutdown, we will write a ``modules-<random>`` file to
+    /// the directory specified containing a ``\n`` delimited list of modules
+    /// loaded in ``sys.modules``.
+    pub write_modules_directory_env: Option<String>,
+
+    /// Defines what code to run by default.
+    ///
+    pub run: PythonRunMode,
+}
+
+impl From<PythonConfig> for OxidizedPythonInterpreterConfig {
+    fn from(config: PythonConfig) -> Self {
+        Self {
+            interpreter_config: PythonInterpreterConfig {
+                profile: if config.isolated {
+                    PythonInterpreterProfile::Isolated
+                } else {
+                    PythonInterpreterProfile::Python
+                },
+                stdio_encoding: config.standard_io_encoding,
+                stdio_errors: config.standard_io_errors,
+                optimization_level: Some(match config.opt_level {
+                    0 => OptimizationLevel::Zero,
+                    1 => OptimizationLevel::One,
+                    2 => OptimizationLevel::Two,
+                    _ => OptimizationLevel::Two,
+                }),
+                module_search_paths: if config.sys_paths.is_empty() {
+                    None
+                } else {
+                    Some(config.sys_paths.iter().map(PathBuf::from).collect::<_>())
+                },
+                bytes_warning: Some(match config.bytes_warning {
+                    0 => BytesWarning::None,
+                    1 => BytesWarning::Warn,
+                    2 => BytesWarning::Raise,
+                    _ => BytesWarning::Raise,
+                }),
+                site_import: Some(config.import_site),
+                user_site_directory: Some(config.import_user_site),
+                use_environment: Some(!config.ignore_python_env),
+                inspect: Some(config.inspect),
+                interactive: Some(config.interactive),
+                legacy_windows_fs_encoding: Some(config.legacy_windows_stdio),
+                legacy_windows_stdio: Some(config.legacy_windows_stdio),
+                write_bytecode: Some(config.write_bytecode),
+                buffered_stdio: Some(!config.unbuffered_stdio),
+                parser_debug: Some(config.parser_debug),
+                quiet: Some(config.quiet),
+                verbose: Some(config.verbose != 0),
+                ..PythonInterpreterConfig::default()
+            },
+            raw_allocator: config.raw_allocator,
+            oxidized_importer: config.use_custom_importlib,
+            filesystem_importer: config.filesystem_importer,
+            packed_resources: Some(config.packed_resources),
+            extra_extension_modules: Some(config.extra_extension_modules),
+            argvb: config.argvb,
+            sys_frozen: config.sys_frozen,
+            sys_meipass: config.sys_meipass,
+            terminfo_resolution: config.terminfo_resolution,
+            write_modules_directory_env: config.write_modules_directory_env,
+            run: config.run,
         }
     }
 }

pyoxidizer/src/py_packaging/config.rs
@@ -80,7 +80,7 @@ impl Default for EmbeddedPythonConfig {
             ignore_environment: true,
             inspect: false,
             interactive: false,
-            isolated: false,
+            isolated: true,
             legacy_windows_fs_encoding: false,
             legacy_windows_stdio: false,
             optimize_level: 0,

commit ebac69295d27d28cc5e87ae70e697b63c838da31

pyembed: properly expand $ORIGIN in sys.path
This had been broken since 044ec18352a8a8dd6c7f307fa8841c0b1c3ea5db
back in April. Yikes! We add test coverage for it this time to ensure
it works.

pyembed/src/config.rs
@@ -159,7 +159,24 @@ impl<'a> OxidizedPythonInterpreterConfig<'a> {
     }
 
     /// Resolve the value to use for module search paths / sys.path.
-    pub fn resolve_module_search_paths(&self) -> Result<&Option<Vec<PathBuf>>, &'static str> {
+    pub fn resolve_module_search_paths(&mut self) -> Result<&Option<Vec<PathBuf>>, &'static str> {
+        let origin = self.ensure_origin()?;
+        let origin_string = origin.display().to_string();
+
+        let paths = match &self.interpreter_config.module_search_paths {
+            Some(paths) => Some(
+                paths
+                    .iter()
+                    .map(|p| {
+                        PathBuf::from(p.display().to_string().replace("$ORIGIN", &origin_string))
+                    })
+                    .collect::<Vec<_>>(),
+            ),
+            None => None,
+        };
+
+        self.interpreter_config.module_search_paths = paths;
+
         Ok(&self.interpreter_config.module_search_paths)
     }
 }

pyembed/src/test/interpreter_config.rs
@@ -65,8 +65,17 @@ fn test_sys_paths_origin() -> Result<()> {
     let mut config = OxidizedPythonInterpreterConfig::default();
     config.interpreter_config.module_search_paths = Some(vec![PathBuf::from("$ORIGIN/lib")]);
 
+    let origin = std::env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_path_buf();
+
     let paths = config.resolve_module_search_paths().unwrap();
-    assert_eq!(paths, &Some(vec![PathBuf::from("$ORIGIN/lib")]));
+    assert_eq!(
+        paths,
+        &Some(vec![PathBuf::from(format!("{}/lib", origin.display()))])
+    );
 
     let py_config: pyffi::PyConfig = (&config).try_into().unwrap();
 