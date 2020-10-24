122e376d481b567ae05cb9a45b623d4c512a9f3e
Gregory Szorc <gregory.szorc@gmail.com>
2020-10-24 16:20:13 -0700

    python-packaging: use '' for a single character string

--- python-packaging/src/location.rs
+++ python-packaging/src/location.rs
@@ -81,7 +81,7 @@ impl TryFrom<&str> for ConcreteResourceLocation {
         if value == "in-memory" {
             Ok(Self::InMemory)
         } else {
-            let parts = value.splitn(2, ":").collect::<Vec<_>>();
+            let parts = value.splitn(2, ':').collect::<Vec<_>>();
 
             if parts.len() != 2 {
                 Err(format!("{} is not a valid resource location", value))
