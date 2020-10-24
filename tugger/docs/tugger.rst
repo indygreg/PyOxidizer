34b15f91ff07eac67537fc814c2a29bfb79cb799
Gregory Szorc <gregory.szorc@gmail.com>
2020-10-24 16:26:08 -0700

    tugger: add project scaffolding
    Last year I created a proof-of-concept tool/crate called Tugger.
    Its canonical home is https://github.com/indygreg/tugger. The purpose
    of Tugger was to hold generic application packaging/distribution
    functionality.
    
    At the time, Tugger was an experiment to play around with Starlark,
    before PyOxidizer was using Starlark.
    
    I haven't touched Tugger in a very long time.
    
    Anyway, my goal is to revive tugger as a project inside of the
    PyOxidizer repository. Rather than import the code wholesale (it needs
    a lot of work), my plan is to effectively cherry pick the good parts
    of Tugger into the new code base. Eventually we'll get to a release
    that we'll call version 0.2.
    
    As part of this, we also add Sphinx scaffolding for Tugger.

---
+++ tugger/docs/tugger.rst
@@ -0,0 +1,11 @@
+Shipping Applications with ``tugger``
+=====================================
+
+Tugger is a standalone project that aims to make it easy to ship
+applications.
+
+Tugger is part of the PyOxidizer Project and is developed inside the
+PyOxidizer repository at https://github.com/indygreg/PyOxidizer.
+
+.. toctree::
+   :maxdepth: 2
---
+++ tugger/src/lib.rs
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/*!
+The `tugger` crate contains functionality for packaging and distributing
+software applications.
+
+The core of `tugger` consists of a set of types for defining packaging
+actions and functions to operate on them. There is a frontend component
+which defines a [Starlark](https://github.com/bazelbuild/starlark)
+dialect for allowing these types to be constructed from user-provided
+configuration files.
+
+Tugger is part of the
+[PyOxidizer](https://github.com/indygreg/PyOxidizer) project. While
+developed in the same repository, Tugger is a generic, standalone
+Rust crate and utility. It just happens to be developed alongside
+PyOxidizer.
+*/
