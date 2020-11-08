5f073e70796d9ca4f4942cd7ecbe2b1c55b5d1b2
Gregory Szorc <gregory.szorc@gmail.com>
2020-11-08 10:38:22 -0800

    release: add new project to automate releasing
    PyOxidizer's release process currently involves a lot of manual work.
    This commit introduces a new `release` project that aims to automate
    the release by providing a simple executable to do that.
    
    The functionality in the new executable is currently very limited. But
    it's a start.

---
+++ release/src/main.rs
@@ -0,0 +1,100 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use {
+    anyhow::{anyhow, Context, Result},
+    cargo_toml::Manifest,
+    git2::Repository,
+    lazy_static::lazy_static,
+    std::path::Path,
+};
+
+lazy_static! {
+    static ref DISABLE_PACKAGES: Vec<&'static str> = vec!["oxidized-importer"];
+}
+
+fn get_workspace_members(path: &Path) -> Result<Vec<String>> {
+    let manifest = Manifest::from_path(path)?;
+    Ok(manifest
+        .workspace
+        .ok_or_else(|| anyhow!("no [workspace] section"))?
+        .members)
+}
+
+fn write_workspace_toml(path: &Path, packages: Vec<String>) -> Result<()> {
+    let members = packages
+        .iter()
+        .map(|x| toml::Value::String(x.to_string()))
+        .collect::<Vec<_>>();
+    let mut workspace = toml::value::Table::new();
+    workspace.insert("members".to_string(), toml::Value::from(members));
+
+    let mut manifest = toml::value::Table::new();
+    manifest.insert("workspace".to_string(), toml::Value::Table(workspace));
+
+    let s =
+        toml::to_string_pretty(&manifest).context("serializing new workspace TOML to string")?;
+    std::fs::write(path, s.as_bytes()).context("writing new workspace Cargo.toml")?;
+
+    Ok(())
+}
+
+fn release() -> Result<()> {
+    let cwd = std::env::current_dir()?;
+
+    let repo = Repository::discover(&cwd).context("finding Git repository")?;
+    let repo_root = repo
+        .workdir()
+        .ok_or_else(|| anyhow!("unable to resolve working directory"))?;
+
+    let workspace_toml = repo_root.join("Cargo.toml");
+    let workspace_packages =
+        get_workspace_members(&workspace_toml).context("parsing workspace Cargo.toml")?;
+
+    let new_workspace_packages = workspace_packages
+        .iter()
+        .filter(|p| !DISABLE_PACKAGES.contains(&p.as_str()))
+        .cloned()
+        .collect::<Vec<_>>();
+
+    if new_workspace_packages != workspace_packages {
+        println!("removing packages from {}", workspace_toml.display());
+        write_workspace_toml(&workspace_toml, new_workspace_packages)
+            .context("writing workspace Cargo.toml")?;
+    }
+
+    let workspace_packages = get_workspace_members(&workspace_toml)?;
+    let workspace_missing_disabled = DISABLE_PACKAGES
+        .iter()
+        .any(|p| !workspace_packages.contains(&p.to_string()));
+
+    if workspace_missing_disabled {
+        println!(
+            "re-adding disabled packages from {}",
+            workspace_toml.display()
+        );
+        let mut packages = workspace_packages.clone();
+        for p in DISABLE_PACKAGES.iter() {
+            packages.push(p.to_string());
+        }
+
+        packages.sort();
+
+        write_workspace_toml(&workspace_toml, packages)?;
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let exit_code = match release() {
+        Ok(()) => 0,
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            1
+        }
+    };
+
+    std::process::exit(exit_code);
+}
