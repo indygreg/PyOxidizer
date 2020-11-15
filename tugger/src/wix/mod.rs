300cf0de732167832e5cdd097b0a8bd1d553c254
Gregory Szorc <gregory.szorc@gmail.com>
2020-11-14 18:05:57 -0800

    tugger: use unified use

--- tugger/src/wix/mod.rs
+++ tugger/src/wix/mod.rs
@@ -11,11 +11,13 @@ mod msi_package;
 mod simple_msi_builder;
 mod wxs_builder;
 
-pub use bundle_builder::WiXBundleInstallerBuilder;
-pub use chain::ChainElement;
-pub use common::{run_candle, run_light, target_triple_to_wix_arch, write_file_manifest_to_wix};
-pub use exe_package::ExePackage;
-pub use installer_builder::WiXInstallerBuilder;
-pub use msi_package::MSIPackage;
-pub use simple_msi_builder::WiXSimpleMSIBuilder;
-pub use wxs_builder::WxsBuilder;
+pub use {
+    bundle_builder::WiXBundleInstallerBuilder,
+    chain::ChainElement,
+    common::{run_candle, run_light, target_triple_to_wix_arch, write_file_manifest_to_wix},
+    exe_package::ExePackage,
+    installer_builder::WiXInstallerBuilder,
+    msi_package::MSIPackage,
+    simple_msi_builder::WiXSimpleMSIBuilder,
+    wxs_builder::WxsBuilder,
+};
