c36a0a8e15bfb41aebfb2a7fa05676aa038fac38
Gregory Szorc <gregory.szorc@gmail.com>
2020-11-29 13:59:28 -0800

    python-packaging: implement license scanning for Python packages
    This commit implements logic for scanning an iterable of
    `PythonResource` and parsing license info from package distribution
    files. In theory, this should enable us to extract license info
    from Python packaging operations and apply license screening in
    PyOxidizer. Although this will likely require more APIs on the
    added type.

--- python-packaging/src/licensing.rs
+++ python-packaging/src/licensing.rs
@@ -2,6 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use {
+    crate::{package_metadata::PythonPackageMetadata, resource::PythonResource},
+    anyhow::{Context, Result},
+    std::collections::BTreeMap,
+};
+
 /// SPDX licenses in Python distributions that are not GPL.
 ///
 /// We store an allow list of licenses rather than trying to deny GPL licenses
@@ -25,4 +31,285 @@ pub struct LicenseInfo {
     pub license_filename: String,
     /// Text of the license.
     pub license_text: String,
+}
+
+/// Defines license information for a Python package.
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct PackageLicenseInfo {
+    /// The Python package who license info is being annotated.
+    pub package: String,
+
+    /// Version string of Python package being annotated.
+    pub version: String,
+
+    /// `License` entries in package metadata.
+    pub metadata_licenses: Vec<String>,
+
+    /// Licenses present in `Classifier: License` entries in package metadata.
+    pub classifier_licenses: Vec<String>,
+
+    /// Texts of licenses present in the package.
+    pub license_texts: Vec<String>,
+
+    /// Texts of NOTICE files in the package.
+    pub notice_texts: Vec<String>,
+}
+
+impl PackageLicenseInfo {
+    /// All license identifiers appearing in this package.
+    ///
+    /// This does not take license file text into consideration.
+    pub fn all_license_identifiers(&self) -> Vec<String> {
+        let mut res = vec![];
+
+        for value in self
+            .metadata_licenses
+            .iter()
+            .chain(self.classifier_licenses.iter())
+        {
+            if !res.contains(value) {
+                res.push(value.to_string());
+            }
+        }
+
+        res
+    }
+
+    /// All SPDX license identifiers appearing in this package.
+    pub fn spdx_licenses(&self) -> Vec<spdx::LicenseId> {
+        self.all_license_identifiers()
+            .iter()
+            .filter_map(|value| spdx::license_id(value))
+            .collect::<Vec<_>>()
+    }
+
+    /// All license identifiers that are not recognized SPDX license identifiers.
+    pub fn non_spdx_licenses(&self) -> Vec<String> {
+        self.all_license_identifiers()
+            .into_iter()
+            .filter(|value| spdx::license_id(value).is_none())
+            .collect::<Vec<_>>()
+    }
+}
+
+/// Obtain Python package license information from an iterable of Python resources.
+///
+/// This will look at `PythonPackageDistributionResource` entries and attempt
+/// to find license information within. It looks for license info in `METADATA`
+/// and `PKG-INFO` files (both the `License` key and the trove classifiers) as
+/// well as well-named files.
+pub fn derive_package_license_infos<'a>(
+    resources: impl Iterator<Item = &'a PythonResource<'a>>,
+) -> Result<Vec<PackageLicenseInfo>> {
+    let mut packages = BTreeMap::new();
+
+    let resources = resources.filter_map(|resource| {
+        if let PythonResource::PackageDistributionResource(resource) = resource {
+            Some(resource)
+        } else {
+            None
+        }
+    });
+
+    for resource in resources {
+        let key = (resource.package.clone(), resource.version.clone());
+
+        let entry = packages.entry(key).or_insert(PackageLicenseInfo {
+            package: resource.package.clone(),
+            version: resource.version.clone(),
+            ..Default::default()
+        });
+
+        // This is a special metadata file. Parse it and attempt to extract license info.
+        if resource.name == "METADATA" || resource.name == "PKG-INFO" {
+            let metadata = PythonPackageMetadata::from_metadata(&resource.data.resolve()?)
+                .context("parsing package metadata")?;
+
+            for value in metadata.find_all_headers("License") {
+                entry.metadata_licenses.push(value.to_string());
+            }
+
+            for value in metadata.find_all_headers("Classifier") {
+                if value.starts_with("License ") {
+                    if let Some(license) = value.split(" :: ").last() {
+                        // In case they forget the part after this.
+                        if license != "OSI Approved" {
+                            entry.classifier_licenses.push(license.to_string());
+                        }
+                    }
+                }
+            }
+        }
+        // This looks like a license file.
+        else if resource.name.starts_with("LICENSE")
+            || resource.name.starts_with("LICENSE")
+            || resource.name.starts_with("COPYING")
+        {
+            let data = resource.data.resolve()?;
+            let license_text = String::from_utf8_lossy(&data);
+
+            entry.license_texts.push(license_text.to_string());
+        }
+        // This looks like a NOTICE file.
+        else if resource.name.starts_with("NOTICE") {
+            let data = resource.data.resolve()?;
+            let notice_text = String::from_utf8_lossy(&data);
+
+            entry.notice_texts.push(notice_text.to_string());
+        }
+        // Else we don't know what to do with this file. Just ignore it.
+    }
+
+    Ok(packages.into_iter().map(|(_, v)| v).collect::<Vec<_>>())
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::resource::{
+            PythonPackageDistributionResource, PythonPackageDistributionResourceFlavor,
+        },
+        std::borrow::Cow,
+        tugger_file_manifest::FileData,
+    };
+
+    #[test]
+    fn test_license_identifiers() {
+        let l = PackageLicenseInfo {
+            metadata_licenses: vec!["BSD-1-Clause".to_string(), "invalid-metadata".to_string()],
+            classifier_licenses: vec![
+                "BSD-1-Clause".to_string(),
+                "BSD-2-Clause".to_string(),
+                "invalid-classifier".to_string(),
+            ],
+            ..Default::default()
+        };
+
+        assert_eq!(
+            l.all_license_identifiers(),
+            vec![
+                "BSD-1-Clause".to_string(),
+                "invalid-metadata".to_string(),
+                "BSD-2-Clause".to_string(),
+                "invalid-classifier".to_string()
+            ]
+        );
+
+        assert_eq!(
+            l.spdx_licenses(),
+            vec![
+                spdx::license_id("BSD-1-Clause").unwrap(),
+                spdx::license_id("BSD-2-Clause").unwrap()
+            ]
+        );
+
+        assert_eq!(
+            l.non_spdx_licenses(),
+            vec![
+                "invalid-metadata".to_string(),
+                "invalid-classifier".to_string()
+            ]
+        );
+    }
+
+    #[test]
+    fn test_derive_package_license_infos_empty() -> Result<()> {
+        let infos = derive_package_license_infos(vec![].iter())?;
+        assert!(infos.is_empty());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_derive_package_license_infos_license_file() -> Result<()> {
+        let resources = vec![PythonResource::PackageDistributionResource(Cow::Owned(
+            PythonPackageDistributionResource {
+                location: PythonPackageDistributionResourceFlavor::DistInfo,
+                package: "foo".to_string(),
+                version: "1.0".to_string(),
+                name: "LICENSE".to_string(),
+                data: FileData::Memory(vec![42]),
+            },
+        ))];
+
+        let infos = derive_package_license_infos(resources.iter())?;
+        assert_eq!(infos.len(), 1);
+
+        assert_eq!(
+            infos[0],
+            PackageLicenseInfo {
+                package: "foo".to_string(),
+                version: "1.0".to_string(),
+                license_texts: vec!["*".to_string()],
+                ..Default::default()
+            }
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_derive_package_license_infos_metadata_licenses() -> Result<()> {
+        let resources = vec![PythonResource::PackageDistributionResource(Cow::Owned(
+            PythonPackageDistributionResource {
+                location: PythonPackageDistributionResourceFlavor::DistInfo,
+                package: "foo".to_string(),
+                version: "1.0".to_string(),
+                name: "METADATA".to_string(),
+                data: FileData::Memory(
+                    "Name: foo\nLicense: BSD-1-Clause\nLicense: BSD-2-Clause\n"
+                        .as_bytes()
+                        .to_vec(),
+                ),
+            },
+        ))];
+
+        let infos = derive_package_license_infos(resources.iter())?;
+        assert_eq!(infos.len(), 1);
+
+        assert_eq!(
+            infos[0],
+            PackageLicenseInfo {
+                package: "foo".to_string(),
+                version: "1.0".to_string(),
+                metadata_licenses: vec!["BSD-1-Clause".to_string(), "BSD-2-Clause".to_string()],
+                ..Default::default()
+            }
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_derive_package_license_infos_metadata_classifiers() -> Result<()> {
+        let resources = vec![PythonResource::PackageDistributionResource(Cow::Owned(
+            PythonPackageDistributionResource {
+                location: PythonPackageDistributionResourceFlavor::DistInfo,
+                package: "foo".to_string(),
+                version: "1.0".to_string(),
+                name: "METADATA".to_string(),
+                data: FileData::Memory(
+                    "Name: foo\nClassifier: License :: OSI Approved\nClassifier: License :: OSI Approved :: BSD-1-Clause\n"
+                        .as_bytes()
+                        .to_vec(),
+                ),
+            },
+        ))];
+
+        let infos = derive_package_license_infos(resources.iter())?;
+        assert_eq!(infos.len(), 1);
+
+        assert_eq!(
+            infos[0],
+            PackageLicenseInfo {
+                package: "foo".to_string(),
+                version: "1.0".to_string(),
+                classifier_licenses: vec!["BSD-1-Clause".to_string()],
+                ..Default::default()
+            }
+        );
+
+        Ok(())
+    }
 }
