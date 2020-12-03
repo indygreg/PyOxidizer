8b070ecb91372629790cd2e45b52e6fbdd30a137
Gregory Szorc <gregory.szorc@gmail.com>
2020-12-02 17:39:11 -0800

    tugger-windows: add module docstring

--- tugger-windows/src/signtool.rs
+++ tugger-windows/src/signtool.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+/*! Interface to `signtool.exe`. */
+
 use {
     crate::X509SigningCertificate,
     anyhow::{anyhow, Context, Result},
