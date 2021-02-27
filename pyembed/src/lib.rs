b1d3cab6359708e7ec5eac119f6ab5cfa1a76063
Gregory Szorc <gregory.szorc@gmail.com>
2021-02-26 20:14:58 -0800

    pyembed: update documented list of required dependencies

--- pyembed/src/lib.rs
+++ pyembed/src/lib.rs
@@ -39,11 +39,12 @@ interfacing.
 **It is an explicit goal of this crate to rely on as few external dependencies
 as possible.** This is because we want to minimize bloat in produced binaries.
 At this time, we have required direct dependencies on published versions of the
-`anyhow`, `lazy_static`, `libc`, `memmap`, `python-packed-resources`, and `uuid`
-crates. On Windows, this list is extended by `memory-module-sys` and `winapi`,
-which are required to support loading DLLs from memory. We also have an optional
-direct dependency on the `jemalloc-sys`, `libmimalloc-sys`, and `snmalloc-sys`
-crates for custom memory allocators.
+`anyhow`, `dunce`, `libc`, `memmap`, `once_cell`, `python-packed-resources`,
+`python-packaging`, `tugger-file-manifest`, and `uuid` crates. On Windows, this
+list is extended by `memory-module-sys` and `winapi`, which are required to
+support loading DLLs from memory. We also have an optional direct dependency
+on the `jemalloc-sys`, `libmimalloc-sys`, and `snmalloc-sys` crates for custom
+memory allocators.
 
 This crate requires linking against a library providing CPython C symbols.
 (This dependency is via the `python3-sys` crate.) On Windows, this library
