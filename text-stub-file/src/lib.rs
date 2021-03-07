409116f49e4d1355df214b1d011e86d15685f8ec
Gregory Szorc <gregory.szorc@gmail.com>
2021-03-07 13:02:04 -0800

    text-stub-file: functionality for parsing .tbd files
    Due to a yaml-rust deficiency, the parsing efficiency is sub-optimal.
    
    Test coverage is not great. Although we do have functional
    tests for parsing `.tbd` files in Apple SDKs. That's arguably the
    testing that matters most.

--- text-stub-file/src/lib.rs
+++ text-stub-file/src/lib.rs
@@ -2,10 +2,175 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+pub mod yaml;
+
+use yaml::*;
+use yaml_rust::ScanError;
+
+/// Version of a TBD document.
+#[derive(Copy, Clone, Debug)]
+pub enum TBDVersion {
+    V1,
+    V2,
+    V3,
+    V4,
+}
+
+/// A parsed TBD record from a YAML document.
+pub enum TBDRecord {
+    V1(TBDVersion1),
+    V2(TBDVersion2),
+    V3(TBDVersion3),
+    V4(TBDVersion4),
+}
+
+/// Represents an error when parsing TBD YAML.
+#[derive(Debug)]
+pub enum ParseError {
+    YamlError(yaml_rust::ScanError),
+    DocumentCountMismatch,
+    Serde(serde_yaml::Error),
+}
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::YamlError(e) => e.fmt(f),
+            Self::DocumentCountMismatch => {
+                f.write_str("mismatch in expected document count when parsing YAML")
+            }
+            Self::Serde(e) => e.fmt(f),
+        }
+    }
+}
+
+impl std::error::Error for ParseError {}
+
+impl From<yaml_rust::ScanError> for ParseError {
+    fn from(e: ScanError) -> Self {
+        Self::YamlError(e)
+    }
+}
+
+impl From<serde_yaml::Error> for ParseError {
+    fn from(e: serde_yaml::Error) -> Self {
+        Self::Serde(e)
+    }
+}
+
+const TBD_V2_DOCUMENT_START: &str = "--- !tapi-tbd-v2";
+const TBD_V3_DOCUMENT_START: &str = "--- !tapi-tbd-v3";
+const TBD_V4_DOCUMENT_START: &str = "--- !tapi-tbd";
+
+/// Parse TBD records from a YAML stream.
+///
+/// Returns a series of parsed records contained in the stream.
+pub fn parse_str(data: &str) -> Result<Vec<TBDRecord>, ParseError> {
+    // serde_yaml doesn't support tags on documents with YAML streams
+    // (https://github.com/dtolnay/serde-yaml/issues/147) because yaml-rust
+    // doesn't do so (https://github.com/chyh1990/yaml-rust/issues/147). Our
+    // extremely hacky and inefficient solution is to parse the stream once
+    // using yaml_rust to ensure it is valid YAML. Then we do a manual pass
+    // scanning for document markers (`---` and `...`) and corresponding TBD
+    // tags. We then pair things up and feed each document into the serde_yaml
+    // deserializer for the given type.
+
+    let yamls = yaml_rust::YamlLoader::load_from_str(data)?;
+
+    // We got valid YAML. That's a good sign. Proceed with document/tag scanning.
+
+    let mut document_versions = vec![];
+
+    for line in data.lines() {
+        // Start of new YAML document.
+        if line.starts_with("---") {
+            let version = if line.starts_with(TBD_V2_DOCUMENT_START) {
+                TBDVersion::V2
+            } else if line.starts_with(TBD_V3_DOCUMENT_START) {
+                TBDVersion::V3
+            } else if line.starts_with(TBD_V4_DOCUMENT_START) {
+                TBDVersion::V4
+            } else {
+                // Version 1 has no document tag.
+                TBDVersion::V1
+            };
+
+            document_versions.push(version);
+        }
+    }
+
+    // The initial document marker in a YAML file is optional. And the
+    // `---` marker is a version 1 TBD. So if there is a count mismatch,
+    // insert a version 1 at the beginning of the versions list.
+    if document_versions.len() == yamls.len() - 1 {
+        document_versions.insert(0, TBDVersion::V1);
+    } else if document_versions.len() != yamls.len() {
+        return Err(ParseError::DocumentCountMismatch);
+    }
+
+    let mut res = vec![];
+
+    for (index, value) in yamls.iter().enumerate() {
+        // TODO We could almost certainly avoid the YAML parsing round trip
+        let mut s = String::new();
+        yaml_rust::YamlEmitter::new(&mut s).dump(value).unwrap();
+
+        res.push(match document_versions[index] {
+            TBDVersion::V1 => TBDRecord::V1(serde_yaml::from_str(&s)?),
+            TBDVersion::V2 => TBDRecord::V2(serde_yaml::from_str(&s)?),
+            TBDVersion::V3 => TBDRecord::V3(serde_yaml::from_str(&s)?),
+            TBDVersion::V4 => TBDRecord::V4(serde_yaml::from_str(&s)?),
+        })
+    }
+
+    Ok(res)
+}
+
 #[cfg(test)]
 mod tests {
+    use {
+        super::*,
+        rayon::prelude::*,
+        tugger_apple::{
+            find_command_line_tools_sdks, find_developer_sdks,
+            find_system_xcode_developer_directories,
+        },
+    };
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_parse_apple_sdk_tbds() {
+        // This will find older Xcode versions and their SDKs when run in GitHub
+        // Actions. That gives us extreme test coverage of real world .tbd files.
+        let mut sdks = find_system_xcode_developer_directories()
+            .unwrap()
+            .into_iter()
+            .map(|p| find_developer_sdks(&p).unwrap())
+            .flatten()
+            .collect::<Vec<_>>();
+
+        if let Some(extra) = find_command_line_tools_sdks().unwrap() {
+            sdks.extend(extra);
+        }
+
+        // Filter out symlinked SDKs to avoid duplicates.
+        let sdks = sdks
+            .into_iter()
+            .filter(|sdk| !sdk.is_symlink)
+            .collect::<Vec<_>>();
+
+        sdks.into_par_iter().for_each(|sdk| {
+            for entry in walkdir::WalkDir::new(&sdk.path) {
+                let entry = entry.unwrap();
+
+                let file_name = entry.file_name().to_string_lossy();
+                if file_name.ends_with(".tbd") {
+                    eprintln!("parsing {}", entry.path().display());
+                    let data = std::fs::read(&entry.path()).unwrap();
+                    let data = String::from_utf8(data).unwrap();
+
+                    parse_str(&data).unwrap();
+                }
+            }
+        });
     }
 }
---
+++ text-stub-file/src/yaml.rs
@@ -0,0 +1,556 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/*! YAML structures in text stub files.
+
+This module defines raw YAML primitives existing in text stub files / `.tbd`
+files.
+
+See https://github.com/llvm/llvm-project/blob/main/llvm/lib/TextAPI/MachO/TextStub.cpp
+for specifications of the YAML files.
+*/
+
+use serde::{Deserialize, Serialize};
+
+/*
+The TBD v1 format only support two level address libraries and is per
+definition application extension safe.
+---                              # the tag !tapi-tbd-v1 is optional and
+                                 # shouldn't be emitted to support older linker.
+archs: [ armv7, armv7s, arm64 ]  # the list of architecture slices that are
+                                 # supported by this file.
+platform: ios                    # Specifies the platform (macosx, ios, etc)
+install-name: /u/l/libfoo.dylib  #
+current-version: 1.2.3           # Optional: defaults to 1.0
+compatibility-version: 1.0       # Optional: defaults to 1.0
+swift-version: 0                 # Optional: defaults to 0
+objc-constraint: none            # Optional: defaults to none
+exports:                         # List of export sections
+...
+Each export section is defined as following:
+ - archs: [ arm64 ]                   # the list of architecture slices
+   allowed-clients: [ client ]        # Optional: List of clients
+   re-exports: [ ]                    # Optional: List of re-exports
+   symbols: [ _sym ]                  # Optional: List of symbols
+   objc-classes: []                   # Optional: List of Objective-C classes
+   objc-ivars: []                     # Optional: List of Objective C Instance
+                                      #           Variables
+   weak-def-symbols: []               # Optional: List of weak defined symbols
+   thread-local-symbols: []           # Optional: List of thread local symbols
+*/
+
+/// Version 1 of the TBD structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion1 {
+    /// The list of architecture slices that are supported by this file.
+    ///
+    /// armv7, arm64, etc.
+    archs: Vec<String>,
+
+    /// Specifies the platform (macosx, ios, etc).
+    platform: String,
+
+    /// Path of installed library.
+    install_name: String,
+
+    /// Current version of library.
+    ///
+    /// Defaults to `1.0`.
+    current_version: Option<String>,
+
+    /// Compatibility version of library.
+    ///
+    /// Defaults to `1.0`.
+    compatibility_version: Option<String>,
+
+    /// Swift version of library.
+    ///
+    /// Defaults to `0`.
+    swift_version: Option<String>,
+
+    /// Objective-C constraint.
+    ///
+    /// Defaults to `none`.
+    objc_constraint: Option<String>,
+
+    /// Export sections.
+    exports: Vec<TBDVersion12ExportSection>,
+}
+
+/// Export section in a TBD version 1 or 2 structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion12ExportSection {
+    /// List of architecture slices.
+    pub archs: Vec<String>,
+
+    /// List of clients.
+    #[serde(default)]
+    pub allowed_clients: Vec<String>,
+
+    /// List of re-exports.
+    #[serde(default)]
+    pub re_exports: Vec<String>,
+
+    /// List of symbols.
+    #[serde(default)]
+    pub symbols: Vec<String>,
+
+    /// List of Objective-C classes.
+    #[serde(default)]
+    pub objc_classes: Vec<String>,
+
+    /// List of Objective-C instance variables.
+    #[serde(default)]
+    pub objc_ivars: Vec<String>,
+
+    /// List of weak defined symbols.
+    #[serde(default)]
+    pub weak_def_symbols: Vec<String>,
+
+    /// List of thread local symbols.
+    #[serde(default)]
+    pub thread_local_symbols: Vec<String>,
+}
+
+/*
+--- !tapi-tbd-v2
+archs: [ armv7, armv7s, arm64 ]  # the list of architecture slices that are
+                                 # supported by this file.
+uuids: [ armv7:... ]             # Optional: List of architecture and UUID pairs.
+platform: ios                    # Specifies the platform (macosx, ios, etc)
+flags: []                        # Optional:
+install-name: /u/l/libfoo.dylib  #
+current-version: 1.2.3           # Optional: defaults to 1.0
+compatibility-version: 1.0       # Optional: defaults to 1.0
+swift-version: 0                 # Optional: defaults to 0
+objc-constraint: retain_release  # Optional: defaults to retain_release
+parent-umbrella:                 # Optional:
+exports:                         # List of export sections
+...
+undefineds:                      # List of undefineds sections
+...
+Each export section is defined as following:
+- archs: [ arm64 ]                   # the list of architecture slices
+  allowed-clients: [ client ]        # Optional: List of clients
+  re-exports: [ ]                    # Optional: List of re-exports
+  symbols: [ _sym ]                  # Optional: List of symbols
+  objc-classes: []                   # Optional: List of Objective-C classes
+  objc-ivars: []                     # Optional: List of Objective C Instance
+                                     #           Variables
+  weak-def-symbols: []               # Optional: List of weak defined symbols
+  thread-local-symbols: []           # Optional: List of thread local symbols
+Each undefineds section is defined as following:
+- archs: [ arm64 ]     # the list of architecture slices
+  symbols: [ _sym ]    # Optional: List of symbols
+  objc-classes: []     # Optional: List of Objective-C classes
+  objc-ivars: []       # Optional: List of Objective C Instance Variables
+  weak-ref-symbols: [] # Optional: List of weak defined symbols
+*/
+
+/// Version 2 of the TBD data structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion2 {
+    /// The list of architecture slices that are supported by this file.
+    pub archs: Vec<String>,
+
+    /// List of architecture and UUID pairs.
+    #[serde(default)]
+    pub uuids: Vec<String>,
+
+    /// Specifies the paltform (macosx, ios, etc).
+    pub platform: String,
+
+    #[serde(default)]
+    pub flags: Vec<String>,
+
+    pub install_name: String,
+
+    /// Current version of library.
+    ///
+    /// Defaults to `1.0`.
+    pub current_version: Option<String>,
+
+    /// Compatibility version of library.
+    ///
+    /// Defaults to `1.0`.
+    pub compatibility_version: Option<String>,
+
+    /// Swift version of library.
+    pub swift_version: Option<String>,
+
+    /// Objective-C constraint.
+    pub objc_constraint: Option<String>,
+
+    pub parent_umbrella: Option<TBDUmbrellaSection>,
+
+    /// Export sections.
+    pub exports: Vec<TBDVersion12ExportSection>,
+
+    /// Undefineds sections.
+    pub undefineds: Vec<TBDVersion2UndefinedsSection>,
+}
+
+/// Undefineds sections in a version 2 TBD structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion2UndefinedsSection {
+    /// The list of architecture slices.
+    pub archs: Vec<String>,
+
+    /// List of symbols.
+    #[serde(default)]
+    pub symbols: Vec<String>,
+
+    /// List of Objective-C classes.
+    #[serde(default)]
+    pub objc_classes: Vec<String>,
+
+    /// List of Objective-C instance variables.
+    #[serde(default)]
+    pub objc_ivars: Vec<String>,
+
+    /// List of weak defined symbols.
+    #[serde(default)]
+    pub weak_ref_symbols: Vec<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDUmbrellaSection {
+    #[serde(default)]
+    pub targets: Vec<String>,
+
+    pub umbrella: String,
+}
+
+/*
+--- !tapi-tbd-v3
+archs: [ armv7, armv7s, arm64 ]  # the list of architecture slices that are
+                                 # supported by this file.
+uuids: [ armv7:... ]             # Optional: List of architecture and UUID pairs.
+platform: ios                    # Specifies the platform (macosx, ios, etc)
+flags: []                        # Optional:
+install-name: /u/l/libfoo.dylib  #
+current-version: 1.2.3           # Optional: defaults to 1.0
+compatibility-version: 1.0       # Optional: defaults to 1.0
+swift-abi-version: 0             # Optional: defaults to 0
+objc-constraint: retain_release  # Optional: defaults to retain_release
+parent-umbrella:                 # Optional:
+exports:                         # List of export sections
+...
+undefineds:                      # List of undefineds sections
+...
+Each export section is defined as following:
+- archs: [ arm64 ]                   # the list of architecture slices
+  allowed-clients: [ client ]        # Optional: List of clients
+  re-exports: [ ]                    # Optional: List of re-exports
+  symbols: [ _sym ]                  # Optional: List of symbols
+  objc-classes: []                   # Optional: List of Objective-C classes
+  objc-eh-types: []                  # Optional: List of Objective-C classes
+                                     #           with EH
+  objc-ivars: []                     # Optional: List of Objective C Instance
+                                     #           Variables
+  weak-def-symbols: []               # Optional: List of weak defined symbols
+  thread-local-symbols: []           # Optional: List of thread local symbols
+Each undefineds section is defined as following:
+- archs: [ arm64 ]     # the list of architecture slices
+  symbols: [ _sym ]    # Optional: List of symbols
+  objc-classes: []     # Optional: List of Objective-C classes
+  objc-eh-types: []                  # Optional: List of Objective-C classes
+                                     #           with EH
+  objc-ivars: []       # Optional: List of Objective C Instance Variables
+  weak-ref-symbols: [] # Optional: List of weak defined symbols
+*/
+
+/// Version 3 of the TBD data structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion3 {
+    /// The list of architecture slices that are supported by this file.
+    pub archs: Vec<String>,
+
+    /// List of architecture and UUID pairs.
+    #[serde(default)]
+    pub uuids: Vec<String>,
+
+    /// Specifies the paltform (macosx, ios, etc).
+    pub platform: String,
+
+    #[serde(default)]
+    pub flags: Vec<String>,
+
+    pub install_name: String,
+
+    /// Current version of library.
+    ///
+    /// Defaults to `1.0`.
+    pub current_version: Option<String>,
+
+    /// Compatibility version of library.
+    ///
+    /// Defaults to `1.0`.
+    pub compatibility_version: Option<String>,
+
+    /// Swift version of library.
+    pub swift_abi_version: Option<String>,
+
+    /// Objective-C constraint.
+    ///
+    /// Defaults to `retain_release`.
+    pub objc_constraint: Option<String>,
+
+    pub parent_umbrella: Option<TBDUmbrellaSection>,
+
+    /// Export sections.
+    #[serde(default)]
+    pub exports: Vec<TBDVersion3ExportSection>,
+
+    /// Undefineds sections.
+    #[serde(default)]
+    pub undefineds: Vec<TBDVersion3UndefinedsSection>,
+}
+
+/// Export section in a TBD version 3 structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion3ExportSection {
+    /// List of architecture slices.
+    pub archs: Vec<String>,
+
+    /// List of clients.
+    #[serde(default)]
+    pub allowed_clients: Vec<String>,
+
+    /// List of re-exports.
+    #[serde(default)]
+    pub re_exports: Vec<String>,
+
+    /// List of symbols.
+    #[serde(default)]
+    pub symbols: Vec<String>,
+
+    /// List of Objective-C classes.
+    #[serde(default)]
+    pub objc_classes: Vec<String>,
+
+    /// List of Objective-C classes with EH.
+    #[serde(default)]
+    pub objc_eh_types: Vec<String>,
+
+    /// List of Objective-C instance variables.
+    #[serde(default)]
+    pub objc_ivars: Vec<String>,
+
+    /// List of weak defined symbols.
+    #[serde(default)]
+    pub weak_def_symbols: Vec<String>,
+
+    /// List of thread local symbols.
+    #[serde(default)]
+    pub thread_local_symbols: Vec<String>,
+}
+
+/// Undefineds sections in a version 3 TBD structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion3UndefinedsSection {
+    /// The list of architecture slices.
+    pub archs: Vec<String>,
+
+    /// List of symbols.
+    #[serde(default)]
+    pub symbols: Vec<String>,
+
+    /// List of Objective-C classes.
+    #[serde(default)]
+    pub objc_classes: Vec<String>,
+
+    /// List of Objective-C classes with EH.
+    #[serde(default)]
+    pub objc_eh_types: Vec<String>,
+
+    /// List of Objective-C instance variables.
+    #[serde(default)]
+    pub objc_ivars: Vec<String>,
+
+    /// List of weak defined symbols.
+    #[serde(default)]
+    pub weak_ref_symbols: Vec<String>,
+}
+
+/*
+--- !tapi-tbd
+tbd-version: 4                              # The tbd version for format
+targets: [ armv7-ios, x86_64-maccatalyst ]  # The list of applicable tapi supported target triples
+uuids:                                      # Optional: List of target and UUID pairs.
+  - target: armv7-ios
+    value: ...
+  - target: x86_64-maccatalyst
+    value: ...
+flags: []                        # Optional:
+install-name: /u/l/libfoo.dylib  #
+current-version: 1.2.3           # Optional: defaults to 1.0
+compatibility-version: 1.0       # Optional: defaults to 1.0
+swift-abi-version: 0             # Optional: defaults to 0
+parent-umbrella:                 # Optional:
+allowable-clients:
+  - targets: [ armv7-ios ]       # Optional:
+    clients: [ clientA ]
+exports:                         # List of export sections
+...
+re-exports:                      # List of reexport sections
+...
+undefineds:                      # List of undefineds sections
+...
+Each export and reexport  section is defined as following:
+- targets: [ arm64-macos ]                        # The list of target triples associated with symbols
+  symbols: [ _symA ]                              # Optional: List of symbols
+  objc-classes: []                                # Optional: List of Objective-C classes
+  objc-eh-types: []                               # Optional: List of Objective-C classes
+                                                  #           with EH
+  objc-ivars: []                                  # Optional: List of Objective C Instance
+                                                  #           Variables
+  weak-symbols: []                                # Optional: List of weak defined symbols
+  thread-local-symbols: []                        # Optional: List of thread local symbols
+- targets: [ arm64-macos, x86_64-maccatalyst ]    # Optional: Targets for applicable additional symbols
+  symbols: [ _symB ]                              # Optional: List of symbols
+Each undefineds section is defined as following:
+- targets: [ arm64-macos ]    # The list of target triples associated with symbols
+  symbols: [ _symC ]          # Optional: List of symbols
+  objc-classes: []            # Optional: List of Objective-C classes
+  objc-eh-types: []           # Optional: List of Objective-C classes
+                              #           with EH
+  objc-ivars: []              # Optional: List of Objective C Instance Variables
+  weak-symbols: []            # Optional: List of weak defined symbols
+ */
+
+/// Version 4 of the TBD data structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion4 {
+    /// The tbd version for format.
+    pub tbd_version: usize,
+
+    /// The list of applicable tapi supported target triples.
+    pub targets: Vec<String>,
+
+    /// List of architecture and UUID pairs.
+    #[serde(default)]
+    pub uuids: Vec<TBDVersion4Uuid>,
+
+    #[serde(default)]
+    pub flags: Vec<String>,
+
+    pub install_name: String,
+
+    /// Current version of library.
+    ///
+    /// Defaults to `1.0`.
+    pub current_version: Option<String>,
+
+    /// Compatibility version of library.
+    ///
+    /// Defaults to `1.0`.
+    pub compatibility_version: Option<String>,
+
+    /// Swift version of library.
+    pub swift_abi_version: Option<String>,
+
+    #[serde(default)]
+    pub parent_umbrella: Vec<TBDUmbrellaSection>,
+
+    #[serde(default)]
+    pub allowable_clients: Vec<TBDVersion4AllowableClient>,
+
+    /// Export sections.
+    #[serde(default)]
+    pub exports: Vec<TBDVersion4ExportSection>,
+
+    /// Reexport sections.
+    #[serde(default)]
+    pub re_exports: Vec<TBDVersion4ExportSection>,
+
+    /// Undefineds sections.
+    #[serde(default)]
+    pub undefineds: Vec<TBDVersion4UndefinedsSection>,
+}
+
+/// A UUID value in a TBD version 4 data structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct TBDVersion4Uuid {
+    pub target: String,
+
+    pub value: String,
+}
+
+/// An allowable client in a TBD version 4 data structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct TBDVersion4AllowableClient {
+    #[serde(default)]
+    targets: Vec<String>,
+    clients: Vec<String>,
+}
+
+/// (Re)export section in a TBD version 4 structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion4ExportSection {
+    /// Target triples associated with symbols.
+    pub targets: Vec<String>,
+
+    /// List of symbols.
+    #[serde(default)]
+    pub symbols: Vec<String>,
+
+    /// List of Objective-C classes.
+    #[serde(default)]
+    pub objc_classes: Vec<String>,
+
+    /// List of Objective-C classes with EH.
+    #[serde(default)]
+    pub objc_eh_types: Vec<String>,
+
+    /// List of Objective-C instance variables.
+    #[serde(default)]
+    pub objc_ivars: Vec<String>,
+
+    /// List of weak defined symbols.
+    #[serde(default)]
+    pub weak_symbols: Vec<String>,
+
+    /// List of thread local symbols.
+    #[serde(default)]
+    pub thread_local_symbols: Vec<String>,
+}
+
+/// Undefineds sections in a version 4 TBD structure.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct TBDVersion4UndefinedsSection {
+    /// The list of target triples associated with symbols.
+    pub targets: Vec<String>,
+
+    /// List of symbols.
+    #[serde(default)]
+    pub symbols: Vec<String>,
+
+    /// List of Objective-C classes.
+    #[serde(default)]
+    pub objc_classes: Vec<String>,
+
+    /// List of Objective-C classes with EH.
+    #[serde(default)]
+    pub objc_eh_types: Vec<String>,
+
+    /// List of Objective-C instance variables.
+    #[serde(default)]
+    pub objc_ivars: Vec<String>,
+
+    /// List of weak defined symbols.
+    #[serde(default)]
+    pub weak_symbols: Vec<String>,
+}
