a3e35afd9987d74abe7fb51bc47e284bbdd25be3
Gregory Szorc <gregory.szorc@gmail.com>
2021-04-23 12:51:11 -0700

    tugger: define a global context
    We've done this for build targets and PyOxidizer to facilitate the
    use of global state inside Starlark since the current version of the
    crate doesn't support global variable access. This is hacky. But it
    appears the only way given current Starlark APIs.
    
    Anyway, this will be used to register our code signers globally
    within the environment.

--- tugger/src/starlark/mod.rs
+++ tugger/src/starlark/mod.rs
@@ -17,7 +17,71 @@ pub mod wix_bundle_builder;
 pub mod wix_installer;
 pub mod wix_msi_builder;
 
-use starlark::environment::{Environment, EnvironmentError, TypeValues};
+use {
+    starlark::{
+        environment::{Environment, EnvironmentError, TypeValues},
+        values::{
+            error::{RuntimeError, ValueError},
+            Immutable, Mutable, TypedValue, Value, ValueResult,
+        },
+    },
+    std::ops::Deref,
+};
+
+/// Holds global context for Tugger Starlark evaluation.
+#[derive(Default)]
+pub struct TuggerContext {}
+
+#[derive(Default)]
+pub struct TuggerContextValue {
+    inner: TuggerContext,
+}
+
+impl Deref for TuggerContextValue {
+    type Target = TuggerContext;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl TypedValue for TuggerContextValue {
+    type Holder = Immutable<TuggerContextValue>;
+    const TYPE: &'static str = "TuggerContext";
+
+    fn values_for_descendant_check_and_freeze(&self) -> Box<dyn Iterator<Item = Value>> {
+        Box::new(std::iter::empty())
+    }
+}
+
+#[derive(Default)]
+pub struct TuggerContextHolder {}
+
+impl TypedValue for TuggerContextHolder {
+    type Holder = Mutable<TuggerContextHolder>;
+    const TYPE: &'static str = "Tugger";
+
+    fn values_for_descendant_check_and_freeze(&self) -> Box<dyn Iterator<Item = Value>> {
+        Box::new(std::iter::empty())
+    }
+}
+
+const ENVIRONMENT_CONTEXT_SYMBOL: &str = "TUGGER_CONTEXT";
+
+pub fn get_context_value(type_values: &TypeValues) -> ValueResult {
+    type_values
+        .get_type_value(
+            &Value::new(TuggerContextHolder::default()),
+            ENVIRONMENT_CONTEXT_SYMBOL,
+        )
+        .ok_or_else(|| {
+            ValueError::from(RuntimeError {
+                code: "TUGGER",
+                message: "unable to resolve context (this should never happen)".to_string(),
+                label: "".to_string(),
+            })
+        })
+}
 
 /// Registers Tugger's Starlark dialect.
 pub fn register_starlark_dialect(
@@ -36,8 +100,31 @@ pub fn register_starlark_dialect(
 
 /// Populate a Starlark environment with variables needed to support this dialect.
 pub fn populate_environment(
-    _env: &mut Environment,
-    _type_values: &mut TypeValues,
+    env: &mut Environment,
+    type_values: &mut TypeValues,
 ) -> Result<(), EnvironmentError> {
+    env.set(
+        ENVIRONMENT_CONTEXT_SYMBOL,
+        Value::new(TuggerContextValue::default()),
+    )?;
+
+    let symbol = &ENVIRONMENT_CONTEXT_SYMBOL;
+    type_values.add_type_value(TuggerContextHolder::TYPE, symbol, env.get(symbol)?);
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, crate::starlark::testutil::*, anyhow::Result};
+
+    #[test]
+    fn test_get_context() -> Result<()> {
+        let env = StarlarkEnvironment::new()?;
+
+        let context_value = get_context_value(&env.type_values).unwrap();
+        context_value.downcast_ref::<TuggerContextValue>().unwrap();
+
+        Ok(())
+    }
 }
