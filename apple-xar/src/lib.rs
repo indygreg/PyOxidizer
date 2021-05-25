81b548fc5c747f9d44208ee2c42dc0767f077e44
Gregory Szorc <gregory.szorc@gmail.com>
2021-05-24 18:30:28 -0700

    apple-xar: implement basic archive reading support
    This is far from feature complete. But I'm able to open and
    extract at least 1 test .pkg file with this code, so it is progress.

--- apple-xar/src/lib.rs
+++ apple-xar/src/lib.rs
@@ -1,7 +1,47 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/*! XAR file format */
+
+pub mod format;
+pub mod reader;
+pub mod table_of_contents;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("(de)serialization error: {0}")]
+    Scroll(#[from] scroll::Error),
+
+    #[error("unable to parse checksum string: {0}")]
+    BadChecksum(&'static str),
+
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("decompression error: {0}")]
+    Decompress(#[from] flate2::DecompressError),
+
+    #[error("XML error: {0}")]
+    SerdeXml(#[from] serde_xml_rs::Error),
+
+    #[error("Invalid file ID")]
+    InvalidFileId,
+
+    #[error("File has no data")]
+    FileNoData,
+
+    #[error("Unimplemented file encoding: {0}")]
+    UnimplementedFileEncoding(String),
+
+    #[error("Operation not supported: {0}")]
+    Unsupported(&'static str),
+
+    #[error("x509 certificate error: {0}")]
+    X509Certificate(#[from] x509_certificate::X509CertificateError),
+
+    #[error("CMS error: {0}")]
+    Cms(#[from] cryptographic_message_syntax::CmsError),
 }
+
+pub type XarResult<T> = std::result::Result<T, Error>;
---
+++ apple-xar/src/table_of_contents.rs
@@ -0,0 +1,274 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! XAR XML table of contents data structure.
+
+use {
+    crate::XarResult,
+    chrono::{DateTime, Utc},
+    serde::{Deserialize, Serialize},
+    std::{
+        io::Read,
+        ops::{Deref, DerefMut},
+    },
+    x509_certificate::{CapturedX509Certificate, X509CertificateError},
+};
+
+/// An XML table of contents in a XAR file.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct TableOfContents {
+    toc: XarToC,
+}
+
+impl Deref for TableOfContents {
+    type Target = XarToC;
+
+    fn deref(&self) -> &Self::Target {
+        &self.toc
+    }
+}
+
+impl DerefMut for TableOfContents {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.toc
+    }
+}
+
+impl TableOfContents {
+    /// Parse XML table of contents from a reader.
+    pub fn from_reader(reader: impl Read) -> XarResult<Self> {
+        Ok(serde_xml_rs::from_reader(reader)?)
+    }
+
+    /// Resolve the complete list of files.
+    ///
+    /// Files are sorted by their numerical ID, which should hopefully also
+    /// be the order that file data occurs in the heap. Each elements consists of
+    /// the full filename and the <file> record.
+    pub fn files(&self) -> Vec<(String, File)> {
+        let mut files = self
+            .toc
+            .files
+            .iter()
+            .flat_map(|f| f.files(None))
+            .collect::<Vec<_>>();
+
+        files.sort_by(|a, b| a.1.id.cmp(&b.1.id));
+
+        files
+    }
+}
+
+/// The main data structure inside a table of contents.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct XarToC {
+    pub creation_time: String,
+    pub checksum: Checksum,
+    #[serde(rename = "file")]
+    pub files: Vec<File>,
+    pub signature: Option<Signature>,
+    pub x_signature: Option<Signature>,
+}
+
+impl XarToC {
+    /// Signatures present in the table of contents.
+    pub fn signatures(&self) -> Vec<&Signature> {
+        let mut res = vec![];
+        if let Some(sig) = &self.signature {
+            res.push(sig);
+        }
+        if let Some(sig) = &self.x_signature {
+            res.push(sig);
+        }
+
+        res
+    }
+
+    /// Attempt to find a signature given a signature style.
+    pub fn find_signature(&self, style: SignatureStyle) -> Option<&Signature> {
+        self.signatures().into_iter().find(|sig| sig.style == style)
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Checksum {
+    /// The digest format used.
+    pub style: ChecksumType,
+
+    /// Offset within heap of the checksum data.
+    pub offset: u64,
+
+    /// Size of checksum data.
+    pub size: u64,
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ChecksumType {
+    None,
+    Sha1,
+    Sha256,
+    Sha512,
+    Md5,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct File {
+    pub id: u64,
+    pub ctime: Option<DateTime<Utc>>,
+    pub mtime: Option<DateTime<Utc>>,
+    pub atime: Option<DateTime<Utc>>,
+    pub name: String,
+    #[serde(rename = "type")]
+    pub file_type: FileType,
+    pub mode: Option<u32>,
+    pub deviceno: Option<u32>,
+    pub inode: Option<u64>,
+    pub uid: Option<u32>,
+    pub gid: Option<u32>,
+    pub user: Option<String>,
+    pub group: Option<String>,
+    pub size: Option<u64>,
+    pub data: Option<FileData>,
+    pub ea: Option<Ea>,
+    #[serde(rename = "FinderCreateTime")]
+    pub finder_create_time: Option<FinderCreateTime>,
+    #[serde(default, rename = "file")]
+    pub files: Vec<File>,
+}
+
+impl File {
+    pub fn files(&self, directory: Option<&str>) -> Vec<(String, File)> {
+        let full_path = if let Some(d) = directory {
+            format!("{}/{}", d, self.name)
+        } else {
+            self.name.clone()
+        };
+
+        let mut files = vec![(full_path.clone(), self.clone())];
+
+        for f in &self.files {
+            files.extend(f.files(Some(&full_path)));
+        }
+
+        files
+    }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum FileType {
+    File,
+    Directory,
+    HardLink,
+    Link,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct FileData {
+    pub offset: u64,
+    pub size: u64,
+    pub length: u64,
+    pub extracted_checksum: FileChecksum,
+    pub archived_checksum: FileChecksum,
+    pub encoding: FileEncoding,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct FileChecksum {
+    pub style: ChecksumType,
+    #[serde(rename = "$value")]
+    pub checksum: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct FileEncoding {
+    pub style: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Ea {
+    pub name: String,
+    pub offset: u64,
+    pub size: u64,
+    pub length: u64,
+    pub extracted_checksum: FileChecksum,
+    pub archived_checksum: FileChecksum,
+    pub encoding: FileEncoding,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct FinderCreateTime {
+    pub nanoseconds: u64,
+    pub time: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Signature {
+    pub style: SignatureStyle,
+    pub offset: u64,
+    pub size: u64,
+    #[serde(rename = "KeyInfo")]
+    pub key_info: KeyInfo,
+}
+
+impl Signature {
+    /// Obtained parsed X.509 certificates.
+    pub fn x509_certificates(&self) -> XarResult<Vec<CapturedX509Certificate>> {
+        self.key_info.x509_certificates()
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum SignatureStyle {
+    /// Cryptographic message syntax.
+    Cms,
+
+    /// RSA signature.
+    Rsa,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct KeyInfo {
+    #[serde(rename = "X509Data")]
+    pub x509_data: X509Data,
+}
+
+impl KeyInfo {
+    /// Obtain parsed X.509 certificates.
+    pub fn x509_certificates(&self) -> XarResult<Vec<CapturedX509Certificate>> {
+        self.x509_data.x509_certificates()
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct X509Data {
+    #[serde(rename = "X509Certificate")]
+    pub x509_certificate: Vec<String>,
+}
+
+impl X509Data {
+    /// Obtain parsed X.509 certificates.
+    pub fn x509_certificates(&self) -> XarResult<Vec<CapturedX509Certificate>> {
+        Ok(self
+            .x509_certificate
+            .iter()
+            .map(|data| {
+                // The data in the XML isn't armored. So we add armoring so it can
+                // be decoded by the pem crate.
+                let data = format!(
+                    "-----BEGIN CERTIFICATE-----\r\n{}\r\n-----END CERTIFICATE-----\r\n",
+                    data
+                );
+
+                CapturedX509Certificate::from_pem(data)
+            })
+            .collect::<Result<Vec<_>, X509CertificateError>>()?)
+    }
+}
