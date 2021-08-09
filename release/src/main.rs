41b4d71c3483dc33fbe58eb24fa0470c4e0a09c2
Gregory Szorc <gregory.szorc@gmail.com>
2021-08-08 17:10:11 -0700

    release: remove tugger-rpm validation skipping
    This reverts commit d3241dd68769cd15ea45c3c727040ac0c5570fe2.

--- release/src/main.rs
+++ release/src/main.rs
@@ -675,18 +675,11 @@ fn release_package(
             package
         );
     } else if publish {
-        // rpm-rs has issues with package dependency pinning.
-        let args = if package == "tugger-rpm" {
-            vec!["publish", "--no-verify"]
-        } else {
-            vec!["publish"]
-        };
-
         if run_cmd(
             package,
             &root.join(package),
             "cargo",
-            args,
+            vec!["publish"],
             vec![format!(
                 "crate version `{}` is already uploaded",
                 release_version
