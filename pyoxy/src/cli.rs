c931ec741c37e7fb48cb1a29c89d49f1f0c73dad
Gregory Szorc <gregory.szorc@gmail.com>
2022-01-30 20:39:30 -0800

    pyoxy: port to clap 3.0
    And this finishes our port to clap 3.0.

--- pyoxy/src/cli.rs
+++ pyoxy/src/cli.rs
@@ -5,7 +5,7 @@
 use {
     crate::{interpreter::run_python, yaml::run_yaml_path},
     anyhow::{anyhow, Context, Result},
-    clap::{App, AppSettings, Arg, SubCommand},
+    clap::{App, AppSettings, Arg},
     std::path::{Path, PathBuf},
 };
 
@@ -32,29 +32,34 @@ fn run_normal(exe: &Path) -> Result<i32> {
         .author("Gregory Szorc <gregory.szorc@gmail.com>");
 
     let app = app.subcommand(
-        SubCommand::with_name("run-python")
+        App::new("run-python")
             .about("Make the executable behave like a `python` executable")
             .arg(
-                Arg::with_name("args")
+                Arg::new("args")
                     .help("Arguments to Python interpreter")
-                    .multiple(true)
+                    .multiple_occurrences(true)
+                    .multiple_values(true)
+                    .allow_invalid_utf8(true)
                     .last(true),
             ),
     );
 
     let app = app.subcommand(
-        SubCommand::with_name("run-yaml")
+        App::new("run-yaml")
             .about("Run a Python interpreter defined via a YAML file")
             .setting(AppSettings::ArgRequiredElseHelp)
             .arg(
-                Arg::with_name("yaml_path")
+                Arg::new("yaml_path")
                     .value_name("FILE")
+                    .allow_invalid_utf8(true)
                     .help("Path to YAML file to evaluate"),
             )
             .arg(
-                Arg::with_name("args")
+                Arg::new("args")
                     .help("Arguments to Python interpreter")
-                    .multiple(true)
+                    .multiple_occurrences(true)
+                    .multiple_values(true)
+                    .allow_invalid_utf8(true)
                     .last(true),
             ),
     );
@@ -62,7 +67,7 @@ fn run_normal(exe: &Path) -> Result<i32> {
     let matches = app.get_matches();
 
     match matches.subcommand() {
-        ("run-python", Some(args)) => {
+        Some(("run-python", args)) => {
             let program_args = args
                 .values_of_os("args")
                 .unwrap_or_default()
@@ -70,7 +75,7 @@ fn run_normal(exe: &Path) -> Result<i32> {
 
             run_python(exe, &program_args)
         }
-        ("run-yaml", Some(args)) => {
+        Some(("run-yaml", args)) => {
             let yaml_path = PathBuf::from(
                 args.value_of_os("yaml_path")
                     .expect("yaml_path should be set"),
