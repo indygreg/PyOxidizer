f102c86c14fd7f5c1aab4fc0a4976d545b6a5278
Gregory Szorc <gregory.szorc@gmail.com>
2022-04-01 22:08:59 -0700

    apple-codesign: properly look for Resources directory
    I broke this a few commits ago when I changed the logic. This needs
    to respect whether we're a shallow bundle or not.
    
    After making this change, non-main Mach-O binaries in the complex/nested
    bundle I'm using as my test subject are properly signed and their code
    directory digests are added to the CodeResources file.
    
    However, our output varies from Apple due to SHA-1 vs SHA-256
    again. That should hopefully be easy enough to fix...

--- apple-codesign/src/bundle_signing.rs
+++ apple-codesign/src/bundle_signing.rs
@@ -411,7 +411,10 @@ impl SingleBundleSigner {
             .ok_or_else(|| AppleCodesignError::BundleNoIdentifier(self.bundle.info_plist_path()))?;
 
         warn!("collecting code resources files");
-        let mut resources_builder = if self.bundle.root_dir().join("Resources").is_dir() {
+        // We need to respect whether the bundle is shallow or not because `Resources/` will be
+        // in the root directory for non-shallow bundles like frameworks and `Contents/Resources`
+        // for non-shallow app bundles.
+        let mut resources_builder = if self.bundle.resolve_path("Resources").is_dir() {
             CodeResourcesBuilder::default_resources_rules()?
         } else {
             CodeResourcesBuilder::default_no_resources_rules()?
