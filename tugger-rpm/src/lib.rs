3c17fad9b34bc4f467b2b1e76e3b8bf59ff9a0b7
Gregory Szorc <gregory.szorc@gmail.com>
2022-07-05 18:25:01 -0700

    tugger-rpm: use rpm-rs 0.8
    The fix was merged upstream and is usable now. So we can enable this.
    The crate is still disabled for historical reasons. I don't want to
    revive it just yet.

--- tugger-rpm/src/lib.rs
+++ tugger-rpm/src/lib.rs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#[cfg(target_family = "unix")]
 mod builder;
 
-#[cfg(target_family = "unix")]
 pub use builder::RpmBuilder;
