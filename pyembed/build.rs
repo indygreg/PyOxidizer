16ec96f5da0b8df253d71d17583cfea96068a8cd
Gregory Szorc <gregory.szorc@gmail.com>
2022-11-06 10:24:16 -0800

    pyembed: set linker arguments in build script
    Just like we did for pyoxy.
    
    This preserves previous behavior from the config file. Just inlines it
    into the build script.

--- pyembed/build.rs
+++ pyembed/build.rs
@@ -10,6 +10,21 @@ fn main() {
         std::env::var("CARGO_MANIFEST_DIR").unwrap()
     );
 
+    // By default Rust will not export dynamic symbols from built executables.
+    // If we're linking libpython, we need its symbols to be exported in order to
+    // load Python extension modules.
+    if let Ok(os) = std::env::var("CARGO_CFG_TARGET_OS") {
+        match os.as_str() {
+            "linux" => {
+                println!("cargo:rustc-link-arg=-Wl,-export-dynamic");
+            }
+            "macos" => {
+                println!("cargo:rustc-link-arg=-rdynamic");
+            }
+            _ => {}
+        }
+    }
+
     let interpreter_config = pyo3_build_config::get();
 
     // Re-export the path to the configured Python interpreter. Tests can
