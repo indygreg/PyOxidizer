bd00bbf949cf520f6c2859239e1868f6ee8d7650
Gregory Szorc <gregory.szorc@gmail.com>
2022-11-07 19:00:05 -0800

    releasebot: release-version-change pyembed 0.23.0-pre -> 0.23.0

--- pyoxidizer/src/environment.rs
+++ pyoxidizer/src/environment.rs
@@ -23,7 +23,7 @@ use {
 const PYOXIDIZER_CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 /// Version string of pyembed crate from its Cargo.toml.
-const PYEMBED_CRATE_VERSION: &str = "0.23.0-pre";
+const PYEMBED_CRATE_VERSION: &str = "0.23.0";
 
 /// URL of Git repository we were built from.
 const GIT_REPO_URL: &str = env!("GIT_REPO_URL");
